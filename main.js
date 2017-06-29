f2cedc891211bc4387ea43efa0f13eb0a5eb729b
Yang Theng <yangtheng92@gmail.com>
2017-06-30 00:52:56 +0800

    added new type of box, added give up key

--- main.js
+++ main.js
@@ -9,31 +9,44 @@ function randomNo (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function addBox () {
+function choosingBox () {
+  var identifier = randomNo(0, 100)
+  if (identifier < 21) {
+    addBox('difficult')
+  } else {
+    addBox('easy')
+  }
+}
+
+function addBox (type) {
   var boxElem = document.createElement('div')
-  boxElem.style.height = '50px'
-  boxElem.style.width = '80px'
-  boxElem.style.backgroundColor = 'pink'
+  boxElem.style.height = '20px'
+  // boxElem.style.width = '80px'
+  boxElem.style.padding = '0 10px'
+  if (type === 'difficult') {
+    boxElem.style.backgroundColor = 'orange'
+  } else {
+    boxElem.style.backgroundColor = 'pink'
+  }
   boxElem.style.border = '1px solid black'
-  boxElem.style.top = randomNo(0,400) + 'px'
-  boxElem.style.left = randomNo(76,676) + 'px'
+  boxElem.style.top = randomNo(8, 458) + 'px'
+  boxElem.style.left = randomNo(274, 872) + 'px'
   boxElem.style.position = 'absolute'
   boxElem.style.textAlign = 'center'
-  boxElem.style.lineHeight = '50px'
+  boxElem.style.lineHeight = '20px'
   boxElem.setAttribute('class', 'box')
   addWord(boxElem)
   container.appendChild(boxElem)
 }
 function addWord (appendTo) {
-  boxSelector = document.querySelector('.box')
-  var wordArr = ['apple', 'people', 'dictionary', 'cloud', 'green']
+  var wordArr = ['apple', 'people', 'dictionary', 'cloud', 'green', 'big bird']
   var initialWord = ''
   initialCheck()
   function initialCheck () {
     var initialCheck = false
     while (initialCheck === false) {
-      initialWord = wordArr[randomNo(0, 4)]
-      console.log (initialWord, randomWord)
+      initialWord = wordArr[randomNo(0, 5)]
+      console.log(initialWord, randomWord)
       if (initialWord !== randomWord) {
         initialCheck = true
       }
@@ -53,8 +66,13 @@ function addWord (appendTo) {
 
 function typeLetter (event) {
   var char = String.fromCharCode(event.which)
+  if (char === 'À'){
+    playerArr.push(char)
+    checkIfMatch()
+  } else
   playerArr.push(char.toLowerCase())
   checkIfMatch()
+  console.log(char)
   console.log(playerArr)
 }
 
@@ -66,18 +84,38 @@ function checkIfMatch () {
       if (i === playerArr.length - 1) {
         spanId.style.color = 'red'
       }
-    } else if (playerArr[i] !== boxSelector.textContent[i]) {
-      playerArr.pop()
-    }
-    if (playerArr.length === boxSelector.textContent.length) {
-      // addWord()
+    } else if (playerArr[i] === 'À') {
       playerArr = []
       container.removeChild(boxSelector)
+    } else if (playerArr[i] !== boxSelector.textContent[i]) {
+      //   if (boxSelector.style.backgroundColor === 'orange'){
+      // <-------- DIFFICULT MODE -------->
+      //     playerArr = []
+      //     for (var i = 0; i < boxSelector.textContent.length; i++) {
+      //       document.getElementById(i).style.color = 'black'
+      //     }
+      //   } else {
+      playerArr.pop()
+    }
+  }
+  if (playerArr.length !== 0) {
+    if (playerArr.length === boxSelector.textContent.length) {
+      if (boxSelector.style.backgroundColor === 'orange') {
+      // <-------- DIFFICULT MODE -------->
+        playerArr = []
+        boxSelector.style.backgroundColor = 'pink'
+        for (var i = 0; i < boxSelector.textContent.length; i++) {
+          document.getElementById(i).style.color = 'black'
+        }
+      } else {
+        playerArr = []
+        container.removeChild(boxSelector)
+      }
     }
   }
 }
 
-addBox()
-setInterval(addBox, 2000)
+choosingBox()
+setInterval(choosingBox, 2000)
 
 document.body.addEventListener('keydown', typeLetter)
