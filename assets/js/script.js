97a9ba965f9cdc45e1ecc9559c59da32378a598f
Rebecca Tay <rebeccatay92@gmail.com>
2017-07-01 22:44:09 +0800

    added all ingredient ids. still need to add in negative margins individually depending on how the ingredient should extend out of the burger

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var anchor = document.querySelector('.anchor')
 body.addEventListener('click', function () {
   var newIngredient = document.createElement('div')
   newIngredient.className = 'ingredients'
-  newIngredient.id = 'patty'
+  newIngredient.id = 'cheese'
   newIngredient.style.marginBottom = "-70px"
   newIngredient.style.zIndex = counter
   anchor.prepend(newIngredient)
