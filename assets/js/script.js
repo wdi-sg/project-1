f7e5d2a90486e6889fc223bec639e5e12fed2989
Rebecca Tay <rebeccatay92@gmail.com>
2017-07-01 10:10:56 +0800

    added wireframe v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 var counter = 1
 var pixels  = 30
 
-var container = document.querySelector('.container')
 var body = document.querySelector('body')
+var div = document.querySelector('div')
 
 body.addEventListener('click', function () {
   var newIngredient = document.createElement('div')
   newIngredient.style.bottom = "30px"
   newIngredient.style.zIndex = counter
-  container.prepend(newIngredient)
+  div.prepend(newIngredient)
 })
