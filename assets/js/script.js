538ae57387c69637b5f911c77edfac0181ba4b92
Rebecca Tay <rebeccatay92@gmail.com>
2017-07-01 14:31:18 +0800

    starting on column layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
 var counter = 1
-var pixels  = 30
-
 var body = document.querySelector('body')
 var div = document.querySelector('div')
 
-body.addEventListener('click', function () {
-  var newIngredient = document.createElement('div')
-  newIngredient.style.marginBottom = "-70px"
-  newIngredient.style.zIndex = counter
-  div.prepend(newIngredient)
-  counter ++
-})
+// body.addEventListener('click', function () {
+//   var newIngredient = document.createElement('div')
+//   newIngredient.style.marginBottom = "-70px"
+//   newIngredient.style.zIndex = counter
+//   div.prepend(newIngredient)
+//   counter ++
+// })
