cf81c37527d629751b3f165ac4a3e537ab5141fb
Yang Theng <yangtheng92@gmail.com>
2017-07-03 00:06:41 +0800

    changed lives to hearts, changed win cond to score, changed background image and added effects when live loss

--- main.js
+++ main.js
@@ -2,92 +2,134 @@ function gameInit () {
   var container = document.querySelector('.container')
   var boxSelector = document.querySelector('.box')
   var playerArr = []
-  var life = 1000
-  var bossLife = 5000
+  var lives = 5
+  var score = 0
   var combo = 0
 
-  function gameSetup (level) {
+  function gameSetup () {
+    setBackground()
     createLifeCounter()
-    createBossLifeCounter()
+    createScoreCounter()
     createComboCounter()
-    life = 1000
-    bossLife = 5000
+    createScoreToProgress()
+    lives = 5
+    score = 0
     combo = 0
     setLife()
-    setBossLife()
+    setScore()
     chooseBox()
   }
 
-  function createComboCounter (){
+  function setBackground () {
+    document.body.background = 'images/levelOneBackground.jpg'
+  }
+
+  function createScoreToProgress () {
+    var scoreToProgress = document.createElement('div')
+    scoreToProgress.setAttribute('class', 'scoreToProgress')
+    scoreToProgress.style.top = '-30px'
+    scoreToProgress.style.width = '300px'
+    scoreToProgress.style.left = '500px'
+    scoreToProgress.style.fontSize = '15pt'
+    // scoreToProgress.style.border = '1px solid black'
+    scoreToProgress.style.position = 'absolute'
+    scoreToProgress.style.textAlign = 'center'
+    container.appendChild(scoreToProgress)
+  }
+
+  function createComboCounter () {
     var comboCounter = document.createElement('div')
     comboCounter.setAttribute('class', 'comboCounter')
-    comboCounter.style.top = '-50px'
     comboCounter.style.left = '0px'
     comboCounter.style.width = '800px'
     comboCounter.style.textAlign = 'center'
     comboCounter.style.fontSize = '30pt'
     comboCounter.style.position = 'absolute'
     comboCounter.style.opacity = '1'
+    comboCounter.style.color = 'white'
     container.appendChild(comboCounter)
   }
 
   function createLifeCounter () {
     var lifeCounter = document.createElement('div')
     lifeCounter.setAttribute('class', 'lifeCounter')
-    lifeCounter.style.top = '100px'
-    lifeCounter.style.left = '200px'
+    lifeCounter.style.top = '0px'
+    lifeCounter.style.left = '-100px'
     lifeCounter.style.position = 'absolute'
-    document.body.appendChild(lifeCounter)
+    lifeCounter.style.height = '500px'
+    lifeCounter.style.width = '60px'
+    lifeCounter.style.textAlign = 'center'
+    // lifeCounter.style.border = '1px solid black'
+    lifeCounter.style.fontSize = '20pt'
+    lifeCounter.textContent = 'Lives'
+    for (var i = 1; i <= 5; i++) {
+      var heart = document.createElement('img')
+      heart.src = 'images/redheart.png'
+      heart.setAttribute('class', 'heart')
+      heart.style.height = '40px'
+      heart.style.width = '40px'
+      heart.style.margin = '1px'
+      lifeCounter.appendChild(heart)
+    }
+    container.appendChild(lifeCounter)
   }
 
-  function createBossLifeCounter () {
-    var bossLifeCounter = document.createElement('div')
-    bossLifeCounter.setAttribute('class', 'bossLifeCounter')
-    bossLifeCounter.style.top = '100px'
-    bossLifeCounter.style.left = '1120px'
-    bossLifeCounter.style.position = 'absolute'
-    document.body.appendChild(bossLifeCounter)
+  function createScoreCounter () {
+    var scoreCounter = document.createElement('div')
+    scoreCounter.setAttribute('class', 'scoreCounter')
+    scoreCounter.style.top = '-50px'
+    scoreCounter.style.left = '0px'
+    scoreCounter.style.width = '600px'
+    scoreCounter.style.fontSize = '30pt'
+    scoreCounter.style.textAlign = 'center'
+    scoreCounter.style.position = 'absolute'
+    container.appendChild(scoreCounter)
   }
 
   function setLife () {
     var lifeCounter = document.querySelector('.lifeCounter')
-    lifeCounter.textContent = 'You: ' + life + ' hp'
-    if (life < 1) {
-      alert ('You lose!')
+    if (lives < 1) {
       container.innerHTML = ''
-      lifeCounter.textContent = 'You: 0 hp'
+      inGameMessage('Game Over!', 1000)
     }
   }
 
-  function setBossLife () {
-    var bossLifeCounter = document.querySelector('.bossLifeCounter')
-    bossLifeCounter.textContent = 'Boss: ' + bossLife + ' hp'
-    if (bossLife < 1) {
-      alert ('You win!')
+  function setScore () {
+    var scoreCounter = document.querySelector('.scoreCounter')
+    scoreCounter.textContent = 'Score: ' + score
+    var scoreToProgress = document.querySelector('.scoreToProgress')
+    var pointsToProgress = 5000 - score
+    scoreToProgress.textContent = pointsToProgress + ' points to next level'
+    if (score > 5000) {
       container.innerHTML = ''
-      bossLifeCounter.textContent = 'Boss: 0 hp'
+      inGameMessage('You Win!', 1000)
     }
   }
 
   function setCombo () {
-    var comboCounter = document.querySelector('.comboCounter')
-    comboCounter.textContent = 'Combo ' + combo
-    comboCounter.style.transition = ''
-    comboCounter.style.opacity = '1'
-    setTimeout(function () {
-      comboCounter.style.opacity = '0'
-      comboCounter.style.transition = 'opacity 1s'
-    }, 300)
-    if (combo % 50 === 0 && combo !== 0) {
-      bossLife -= 1000
-      setBossLife()
-      inGameMessage('Nice!')
+    if (combo >= 10) {
+      var comboCounter = document.querySelector('.comboCounter')
+      comboCounter.textContent = 'Combo ' + combo
+      comboCounter.style.top = '50px'
+      comboCounter.style.transition = ''
+      comboCounter.style.opacity = '1'
+      setTimeout(function () {
+        comboCounter.style.opacity = '0'
+        comboCounter.style.transition = 'opacity 1s, top 0.1s'
+        comboCounter.style.top = '45px'
+      }, 10)
+      if (combo % 50 === 0 && combo !== 0) {
+        score += 1000
+        setScore()
+        inGameMessage('Nice!', 10)
+      }
     }
   }
 
-  function inGameMessage (message) {
+  function inGameMessage (message, durationInMs) {
     var inGameMessage = document.createElement('div')
     inGameMessage.textContent = message
+    inGameMessage.style.color = 'white'
     inGameMessage.style.fontSize = '100pt'
     inGameMessage.style.position = 'absolute'
     inGameMessage.style.height = '500px'
@@ -98,7 +140,7 @@ function gameInit () {
     container.appendChild(inGameMessage)
     setTimeout(function () {
       inGameMessage.style.opacity = '0'
-    }, 10)
+    }, durationInMs)
   }
 
   function randomNo (min, max) {
@@ -108,7 +150,7 @@ function gameInit () {
   }
 
   function chooseBox () {
-    if (life > 0 && bossLife > 0) {
+    if (lives > 0 && score < 5000) {
       var identifier = randomNo(0, 100)
       if (identifier < 21) {
         addBox('twoLayer')
@@ -132,14 +174,13 @@ function gameInit () {
     boxElem.style.transition = 'top 5s linear'
     if (type === 'twoLayer') {
       boxElem.style.backgroundColor = 'orange'
-      boxElem.style.transition = 'top 4s linear'
       boxElem.isSecondLayer = false
     } else if (type === 'upsideDown') {
       boxElem.style.backgroundColor = 'cyan'
     } else {
       boxElem.style.backgroundColor = 'pink'
     }
-    boxElem.style.border = '1px solid black'
+    // boxElem.style.border = '1px solid white'
     boxElem.style.top = '0px'
     boxElem.style.left = randomNo(0, 700) + 'px'
     boxElem.style.position = 'absolute'
@@ -192,11 +233,31 @@ function gameInit () {
     container.removeChild(boxToRemove)
     playerArr = []
     if (!boxToRemove.successfulClear) {
-      life -= randomNo(45, 85)
-      setLife()
+      loseLife()
       combo = 0
-      setCombo()
     }// if (!document.querySelector('.box')) addBox('easy')
+  }
+
+  function loseLife () {
+    lives -= 1
+    var heartToRemove = document.querySelector('.heart')
+    var lifeCounter = document.querySelector('.lifeCounter')
+    lifeCounter.removeChild(heartToRemove)
+    var redBackground = document.createElement('div')
+    redBackground.style.height = '100%'
+    redBackground.style.width = '100%'
+    redBackground.style.backgroundColor = 'red'
+    redBackground.style.opacity = '0.5'
+    redBackground.style.transition = 'opacity 0.5s'
+    redBackground.style.position = 'absolute'
+    setTimeout(function() {
+      redBackground.style.opacity = '0'
+    }, 10)
+    setTimeout(function() {
+      document.body.removeChild(redBackground)
+    }, 1000)
+    document.body.appendChild(redBackground)
+    setLife()
   }
 
   function typeLetter (event) {
@@ -226,7 +287,6 @@ function gameInit () {
         setCombo()
       } else if (playerArr[playerLastChar] === '`') {
         removeBox()
-        life -= randomNo(45, 85)
         setLife()
         combo = 0
         setCombo()
@@ -249,8 +309,8 @@ function gameInit () {
         }
       } else {
         boxSelector.successfulClear = true
-        bossLife -= randomNo(75, 115)
-        setBossLife()
+        score += randomNo(75, 115)
+        setScore()
         return true
       }
     } else {
