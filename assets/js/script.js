54f5d881130c9a2f1360d75025ca3bca3efbbe84
Rebecca Tay <rebeccatay92@gmail.com>
2017-07-02 22:42:07 +0800

    testing countdown function. needs a click to start feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,17 @@ body.addEventListener('click', function () {
   var newIngredient = document.createElement('div')
   newIngredient.className = 'ingredients'
   newIngredient.id = 'tomato'
-  newIngredient.style.zIndex = counter
-  playArea.prepend(newIngredient)
+  newIngredient.style.zIndex = counter //increasing z index to make them overlap
+  playArea.prepend(newIngredient) //add before the anchor bottom bun
   counter ++
 })
+
+//need a click to start timer going.
+var timeLeft = 60;
+var time = document.querySelector('.time')
+function countdown() {
+  if (timeLeft > 0) timeLeft --
+  time.innerText = timeLeft + " secs"
+}
+
+setInterval(countdown, 1000)
