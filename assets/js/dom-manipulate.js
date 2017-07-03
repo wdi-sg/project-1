b354b9d4d6ed778c33b0bc1f4f5a250695a9738e
Wanzhen Ng <ngwanzhen@gmail.com>
2017-07-03 12:37:00 +0800

    manipulate img

--- assets/js/dom-manipulate.js
+++ assets/js/dom-manipulate.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', init)
 
+//global var, accessible by both logic and dom?
 var start = [
   ['S','LT',0,0],
   [0,'LR',0,0],
@@ -9,27 +10,35 @@ var start = [
 
 var play = start
 
+function createGrid () {
+  var container = document.querySelector('#container')
+  for (var i = 0; i < start[0].length; i++) {
+    for (var j = 0; j < start.length; j++) {
+      var allDiv = document.createElement('div')
+        var block = document.createElement('img')
+        block.src =
+        'assets/images/concrete small.png'
+        block.alt = '0'
+        var pipe = document.createElement('img')
+        pipe.src =
+        'assets/images/pip.png'
+        pipe.alt = 'LT'
+      allDiv.id = [i] + [j] //assigning id based on natural index
+      //allDiv.textContent = start[i][j] //assigning value based on start grid
+      if (start[i][j] == 0) {allDiv.appendChild(block)}
+      if (start[i][j] !== 0) {allDiv.appendChild(pipe)}
+      allDiv.classList.add('box')
+      container.appendChild(allDiv)
+    }
+  }
+}
+
+createGrid()
 
 function init () {
   // create game board
   var logicFile = logic()
 
-  createGrid()
-
-  console.log(logic.start);
-
-  function createGrid () {
-    var container = document.querySelector('#container')
-    for (var i = 0; i < start[0].length; i++) {
-      for (var j = 0; j < start.length; j++) {
-        var allDiv = document.createElement('div')
-        allDiv.id = [i] + [j] //assigning id based on natural index
-        allDiv.textContent = start[i][j] //assigning value based on start grid
-        allDiv.classList.add('box')
-        container.appendChild(allDiv)
-      }
-    }
-  }
 
 
     //fetch logic and init
@@ -43,58 +52,43 @@ function init () {
       e.addEventListener('click', updateGrid)
     })
 
-    // move tile
+    // Update tiles
 
     function updateGrid () {
-      console.log(play);
-      var k = 0
-      while (k < 16) {
-        for (var i = 0; i < start[0].length; i++) {
-          for (var j = 0; j < start.length; j++) {
-          allDiv[k].textContent = play[i][j]
-          k++
+      //console.log(play);
+      //console.log(allDiv);
+    var imageInDiv2 = document.querySelector('.box:nth-child(1) img')
+        var k = 1
+        while (k < 2) {
+          for (var i = 0; i < start[0].length; i++) {
+            for (var j = 0; j < start.length; j++) {
+              var query = '.box:nth-child(' + k + ')' + ' img'
+              var imageInDiv = document.querySelector(query)
+              if (play[i][j] == 0) {
+                imageInDiv.src =
+                'assets/images/concrete small.png'
+              }
+              if (play[i][j] !== 0) {
+                imageInDiv.src =
+                'assets/images/pip.png'
+              }
+             k++
+              }
             }
           }
         }
-      }
+
+
+    function imgGrid () {
+      for (var k = 0; k < 16; k++) {
+        //if (allDiv[k].textContent == '0') {
+        }
+      //}
+    }
 
     return {
-      updateGrid: updateGrid
+      updateGrid: updateGrid,
+      imgGrid: imgGrid
     }
 
 } // init ends here
-
-//logic file
-function logic () {
-  console.log('logic loaded');
-  // var grid = [
-  //   [0,0,0,0]
-  //   [0,0,0,0]
-  //   [0,0,0,0]
-  //   [0,0,0,0]
-  // ]
-
-
-
-  //if clicked, trigger this func
-  //assuming clicked
-
-
-function moveTileRight (target) {
-    // what you clicked on, to be replaced by click
-    target = event.target
-    var tempValue = target.textContent //stores value e.g. LT
-    var tempID = target.id //stores value e.g. 01
-    var x = parseInt(tempID.substr(0,1))
-    var y = parseInt(tempID.substr(1))
-    play[x][y+1]  = tempValue
-    play[x][y] = 0
-    console.log(play);
-  }
-
-  return {
-    moveTileRight: moveTileRight
-  }
-
-
-}//end logic
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,7 @@
-!!!! STOPPED AT HOW TO SPLIT THE FILES UP, AND CREATED A RIGHT MOVE ONLY, NEED TO ADD UP DOWN LEFT, AND CHECK IF VALID MOVE (0)
-
-!!! THINK ABOUT HOW TO MAKE IT AN IMAGE (can use fonts with nice curves)
-
-!!! how to make animation curved... (use a layering technique so the entire thing expands, but u only see a z-index portion?)
+// !!!! STOPPED AT HOW TO SPLIT THE FILES UP, AND CREATED A RIGHT MOVE ONLY, NEED TO ADD UP DOWN LEFT, AND CHECK IF VALID MOVE (0)
+//
+// !!! THINK ABOUT HOW TO MAKE IT AN IMAGE (can use fonts with nice curves)
+//
 
 
 //logic file
@@ -15,25 +14,19 @@ function logic () {
   //   [0,0,0,0]
   // ]
 
-
-
-  //if clicked, trigger this func
-  //assuming clicked
-
-
-  function moveTileRight () {
+function moveTileRight (target) {
     // what you clicked on, to be replaced by click
-    var target = play[0][1]
-    var temp = target
-    console.log(temp);
-    //var row = start.indexOf(target)
-    //var col = start[row].indexOf(target)
-    //start[row][col] or [col+1] to move
-    start[0][1+1] = temp
-    start[0][1] = 0
-  }
-
-  return {
+    targetDiv = event.currentTarget
+    targetImg = event.target
+    var tempValue = targetImg.alt //stores value e.g. LT
+    var tempID = targetDiv.id //stores value e.g. 01
+    var x = parseInt(tempID.substr(0,1))
+    var y = parseInt(tempID.substr(1))
+    play[x][y+1]  = tempValue
+    play[x][y] = 0
+    console.log(play);
+    }
+    return {
     moveTileRight: moveTileRight
   }
 
