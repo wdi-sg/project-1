b8338e7c982295d0c4d08494bf0e59ddd6ec60be
Rebecca Tay <rebeccatay92@gmail.com>
2017-07-03 09:47:21 +0800

    buggy button event listeners. with forEach, cant extract classname becuz this points to array, not indiv button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,34 @@
 var counter = 1
 var body = document.querySelector('body')
 var playArea = document.querySelector('.playArea')
+var buttons = document.querySelectorAll('.buttons')
 
-body.addEventListener('click', function () {
-  var newIngredient = document.createElement('div')
-  newIngredient.className = 'ingredients'
-  newIngredient.id = 'tomato'
-  newIngredient.style.zIndex = counter //increasing z index to make them overlap
-  playArea.prepend(newIngredient) //add before the anchor bottom bun
-  counter ++
+//add event listeners to all buttons
+buttons.forEach(function(el) {
+  el.addEventListener('click', function() {
+    whichIngredient = el.className
+    console.log(whichIngredient)
+    var newIngredient = document.createElement('div')
+    newIngredient.id =
+    newIngredient.style.zIndex = counter
+    playArea.prepend(newIngredient)
+    counter++
+  })
 })
+
+//callback for button click which extracts classname
+
 
 //need a click to start timer going.
 var timeLeft = 60;
 var time = document.querySelector('.time')
 function countdown() {
-  if (timeLeft > 0) timeLeft --
+  if (timeLeft > 0)
   time.innerText = timeLeft + " secs"
+  timeLeft --
 }
 
-setInterval(countdown, 1000)
+timer = document.querySelector('.timer')
+timer.addEventListener('click', function() {
+  setInterval(countdown, 1000)
+})
