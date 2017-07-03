7f9ce35cf4ec36051525036bd180021597c9d33d
Rebecca Tay <rebeccatay92@gmail.com>
2017-07-03 10:21:32 +0800

    event listeners for buttons now work. later on need to include conditional that adds only if ingredient is correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,39 @@
-var counter = 1
+var counter = 1 // number of ingredients already in play
 var body = document.querySelector('body')
 var playArea = document.querySelector('.playArea')
-var buttons = document.querySelectorAll('.buttons')
+var buttons = document.querySelectorAll('.buttons div')
 
-//add event listeners to all buttons
-buttons.forEach(function(el) {
-  el.addEventListener('click', function() {
-    whichIngredient = el.className
-    console.log(whichIngredient)
-    var newIngredient = document.createElement('div')
-    newIngredient.id =
-    newIngredient.style.zIndex = counter
-    playArea.prepend(newIngredient)
-    counter++
-  })
+// add event listeners to all buttons
+buttons.forEach(function (el) {
+  el.addEventListener('click', addIngredient)
 })
 
-//callback for button click which extracts classname
+// callback for button click which extracts classname
+function addIngredient () {
+   // whichIngredient takes out the classnames 'cheese', 'patty' etc.
+  whichIngredient = this.className.substring(5).toLowerCase()
+  var newIngredient = document.createElement('div')
+   // give the new element classname ingredients, which sets core css
+  newIngredient.className = 'ingredients'
+   // id sets background-image url and negative margins
+  newIngredient.id = whichIngredient
+   // increasing z-index for overlapping look
+  newIngredient.style.zIndex = counter
+  playArea.prepend(newIngredient)
+  counter++
+}
 
-
-//need a click to start timer going.
-var timeLeft = 60;
+// need a click to start timer going.
+var timeLeft = 60
 var time = document.querySelector('.time')
-function countdown() {
-  if (timeLeft > 0)
-  time.innerText = timeLeft + " secs"
-  timeLeft --
+function countdown () {
+  if (timeLeft > 0) {
+    time.innerText = timeLeft + ' secs'
+  }
+  timeLeft--
 }
 
 timer = document.querySelector('.timer')
-timer.addEventListener('click', function() {
+timer.addEventListener('click', function () {
   setInterval(countdown, 1000)
 })
