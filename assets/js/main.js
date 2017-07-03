94f4a00e621352edb9a2e96be05967034cc30aa2
Daniel Lim <ddanielnp@hotmail.com>
2017-07-03 22:44:22 +0800

    mods to DOM and CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,29 @@
 document.addEventListener('DOMContentLoaded', init)
 
 function init () {
-  var cucumber = document.querySelector('.cucumber')
-  var meat = document.querySelector('.meat')
-  var onion = document.querySelector('.onion')
-  var lettuce = document.querySelector('.lettuce')
-  var cheese = document.querySelector('.cheese')
+  var u1 = document.getElementById('u1')
+  var u2 = document.getElementById('u2')
+  var u3 = document.getElementById('u3')
+  var u4 = document.getElementById('u4')
+  var u5 = document.getElementById('u5')
+  var u6 = document.getElementById('u6')
+  var u7 = document.getElementById('u7')
   var hamItem = document.querySelectorAll('.hamItem')
-  console.log(hamItem);
+  // var index = hamItem.length -1
+  console.log(index);
   // console.log(hamItem[hamItem.length - 1]);
   // console.log(hamItem.length - 1);
 
 
   document.addEventListener('keyup', onKeyUp)
-
-  var index = 4
+var index = 4
   function onKeyUp (event) {
     if (checkKeyCode(event.keyCode)) {
       if (event.keyCode === 81) {
         hamItem[index].classList.remove('hamItem')
         hamItem[index].classList.add('cucumber')
         index--
+        console.log(index)
       }
       if (event.keyCode === 87) {
         hamItem[index].classList.remove('hamItem')
@@ -29,22 +32,49 @@ function init () {
         // console.log(index);
       }
       if (event.keyCode === 69) {
-        hamItem[hamItem.length - 1].classList.add('onion')
-        hamItem[hamItem.length - 1].classList.remove('hamItem')
+        hamItem[index].classList.add('onion')
+        hamItem[index].classList.remove('hamItem')
+        index--
       }
       if (event.keyCode === 82) {
-        hamItem[hamItem.length - 1].classList.add('lettuce')
-        hamItem[hamItem.length - 1].classList.remove('hamItem')
+        hamItem[index].classList.add('lettuce')
+        hamItem[index].classList.remove('hamItem')
+        index--
       }
       if (event.keyCode === 84) {
-        hamItem[hamItem.length - 1].classList.add('cheese')
-        hamItem[hamItem.length - 1].classList.remove('hamItem')
+        hamItem[index].classList.add('cheese')
+        hamItem[index].classList.remove('hamItem')
+        index--
       }
-      if (event.keyCode === 32) alert('serve')
+      if (event.keyCode === 32) {
+        document.getElementById('u1').classList.add('topbun')
+        setTimeout(function(){resetClass()},500);
+      }
     } else {
       alert('wrong button pressed')
     }
   }// closing for onKeyUp
+
+  function resetClass () {
+    u1.classList.remove('topbun')
+    u2.classList = 'hamItem'
+    u3.classList = 'hamItem'
+    u4.classList = 'hamItem'
+    u5.classList = 'hamItem'
+    u6.classList = 'hamItem'
+    index = 4
+  }
+
+  // for generating hamburger display
+  var d1 = document.getElementById('d1')
+  var d2 = document.getElementById('d2')
+  var d3 = document.getElementById('d3')
+  var d4 = document.getElementById('d4')
+  var d5 = document.getElementById('d5')
+  var d6 = document.getElementById('d6')
+  var d7 = document.getElementById('d7')
+
+
 
   function checkKeyCode (keycode) {
     if (keycode === 81 || keycode === 87 || keycode === 69 || keycode === 82 || keycode === 84 || keycode === 32) {
--- assets/js/script.js
+++ assets/js/script.js
@@ -43,11 +43,26 @@ function gamePlay () {
 // function to create randomIngredient
 function randomizeIngredient () {
   var ingredient = Math.floor(Math.random() * 5)
-  if (ingredient === 0) randomArray.push('cucumber')
-  if (ingredient === 1) randomArray.push('meat')
-  if (ingredient === 2) randomArray.push('onion')
-  if (ingredient === 3) randomArray.push('lettuce')
-  if (ingredient === 4) randomArray.push('cheese')
+  if (ingredient === 0) {
+    randomArray.push('cucumber')
+    return 'cucumber'
+  }
+  if (ingredient === 1) {
+    randomArray.push('meat')
+    return 'meat'
+  }
+  if (ingredient === 2) {
+    randomArray.push('onion')
+    return 'onion'
+  }
+  if (ingredient === 3) {
+    randomArray.push('lettuce')
+    return 'lettuce'
+  }
+  if (ingredient === 4) {
+    randomArray.push('cheese')
+    return 'cheese'
+  }
 }
 
 function chooseIngredient (food) {
@@ -135,59 +150,3 @@ function resetGame () {
   var playerFoodInput = []
   var displayFoodInput = []
 }
-
-// // target the div id's
-// var d1 = document.getElementById('d1')
-// var d2 = document.getElementById('d2')
-// var d3 = document.getElementById('d3')
-// var d4 = document.getElementById('d4')
-// var d5 = document.getElementById('d5')
-// var d6 = document.getElementById('d6')
-// var d7 = document.getElementById('d7')
-//
-// // function for creating a random displayed burger level 1
-// function createBurgerLv1 () {
-//   d1.classList.add('topbun')
-//   d2.classList.add('randomizeIngredient()')
-//   d3.classList.add('randomizeIngredient()')
-//   d4.classList.add('botbun')
-// }
-//
-// // function for creating a random displayed burger level 2
-// function createBurgerLv2 () {
-//   d1.classList.add('topbun')
-//   d2.classList.add('randomizeIngredient()')
-//   d3.classList.add('randomizeIngredient()')
-//   d4.classList.add('randomizeIngredient()')
-//   d5.classList.add('botbun')
-// }
-//
-// // function for creating a random displayed burger level 3
-// function createBurgerLv3 () {
-//   d1.classList.add('topbun')
-//   d2.classList.add('randomizeIngredient()')
-//   d3.classList.add('randomizeIngredient()')
-//   d4.classList.add('randomizeIngredient()')
-//   d5.classList.add('randomizeIngredient()')
-//   d6.classList.add('botbun')
-// }
-//
-// // function for creating a random displayed burger level 4
-// function createBurgerLv4 () {
-//   d1.classList.add('topbun')
-//   d2.classList.add('randomizeIngredient()')
-//   d3.classList.add('randomizeIngredient()'); d4.classList.add('randomizeIngredient()')
-//   d5.classList.add('randomizeIngredient()')
-//   d6.classList.add('randomizeIngredient()')
-//   d7.classList.add('botbun')
-// }
-//
-// function removeIngredients () {
-//   d1.className = ''
-//   d2.className = ''
-//   d3.className = ''
-//   d4.className = ''
-//   d5.className = ''
-//   d6.className = ''
-//   d7.className = ''
-// }
