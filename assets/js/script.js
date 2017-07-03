a96353cf10a77230a7ff5622f0cec7037ae452ec
shumin huang <ms.huang.shumin@gmail.com>
2017-07-03 14:39:24 +0800

    ghosts movement

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,12 @@ var blueGhost = document.querySelector('#blueGhost')
 var orangeGhost = document.querySelector('#orangeGhost')
 var pinkGhost = document.querySelector('#pinkGhost')
 var redGhost = document.querySelector('#redGhost')
+var ghosts = [blueGhost, orangeGhost, pinkGhost, redGhost]
+
+blueGhost.direction = 'right'
+orangeGhost.direction = 'left'
+pinkGhost.direction = 'left'
+redGhost.direction = 'left'
 
 pacman.style.top = '440px'
 pacman.style.left = '280px'
@@ -19,22 +25,22 @@ blueGhost.style.height = '40px'
 blueGhost.style.width = '40px'
 
 orangeGhost.style.top = '40px'
-orangeGhost.style.left = '160px'
+orangeGhost.style.left = '520px'
 orangeGhost.style.height = '40px'
 orangeGhost.style.width = '40px'
 
-pinkGhost.style.top = '40px'
-pinkGhost.style.left = '80px'
+pinkGhost.style.top = '440px'
+pinkGhost.style.left = '40px'
 pinkGhost.style.height = '40px'
 pinkGhost.style.width = '40px'
 
-redGhost.style.top = '40px'
-redGhost.style.left = '120px'
+redGhost.style.top = '440px'
+redGhost.style.left = '520px'
 redGhost.style.height = '40px'
 redGhost.style.width = '40px'
 
 var pacmanSpeed = 8
-var blueGhostSpeed = 10
+var ghostSpeed = 8
 var walls = []
 
 var upKeyDown = false
@@ -44,12 +50,11 @@ var rightKeyDown = false
 
 // border
 createWall(0, 0, 600, 40)
-createWall(0, 0, 40, 240)
+createWall(0, 0, 40, 280)
 createWall(0, 280, 40, 240)
 createWall(0, 480, 600, 40)
-createWall(560, 0, 40, 240)
+createWall(560, 0, 40, 280)
 createWall(560, 280, 40, 240)
-
 // gameboard tiles
 // W
 createWall(80, 80, 40, 120)
@@ -142,33 +147,41 @@ function hitWall (character) {
   }
 }
 
+// check for collision between the character and the ghost
+function hitGhost (character) {
+  for (var i = 0; i < ghosts.length; i++) {
+    if (collision(ghosts[i], character) && character !== ghosts[i]) {
+      return true
+    }
+  }
+}
+
 // pacman's movement
 function pacmanMovement () {
   var originalLeft = pacman.style.left
   var originalTop = pacman.style.top
+  var pacmanX = parseInt(pacman.style.left)
+  var pacmanY = parseInt(pacman.style.top)
+
   // up key
   if (upKeyDown) {
     pacman.className = 'rotate270'
-    var pacmanY = parseInt(pacman.style.top)
-    pacman.style.top = pacmanY  - pacmanSpeed + 'px'
+    pacman.style.top = pacmanY - pacmanSpeed + 'px'
   }
   // down key
   if (downKeyDown) {
     pacman.className = 'rotate90'
-    var pacmanY = parseInt(pacman.style.top)
     pacman.style.top = pacmanY + pacmanSpeed + 'px'
   }
   // left key
   if (leftKeyDown) {
     pacman.className = 'mirrorImage'
-    var pacmanX = parseInt(pacman.style.left)
     if (pacmanX < 0) pacmanX = 560
     pacman.style.left = pacmanX - pacmanSpeed + 'px'
   }
   // right key
   if (rightKeyDown) {
     pacman.className = ''
-    var pacmanX = parseInt(pacman.style.left) + pacmanSpeed
     if (pacmanX > 560) pacmanX = 0
     pacman.style.left = pacmanX + pacmanSpeed + 'px'
   }
@@ -177,25 +190,76 @@ function pacmanMovement () {
     pacman.style.left = originalLeft
     pacman.style.top = originalTop
   }
-}
-
-// blue ghost's movement
-function blueGhostMovement () {
-  var blueGhostX = parseInt(blueGhost.style.left)
-  if (blueGhostX < 0) blueGhostX = 560
-  if (blueGhostX > 560) blueGhostX = 0
-  blueGhost.style.left = blueGhostX + blueGhostSpeed + 'px'
-  if (hitWall(blueGhost)) blueGhost.style.left = blueGhostX + 'px'
-
-  // check for collision between pacman and blueGhost
-  if (collision(pacman, blueGhost)) {
+  if (hitGhost(pacman)) {
     clearInterval(loopTimer)
   }
+}
+
+// ghost's movement
+function ghostMovement (character) {
+  var charOriginalLeft = character.style.left
+  var charOriginalTop = character.style.top
+  var characterX = parseInt(character.style.left)
+  var characterY = parseInt(character.style.top)
+  // var direction =
+
+// console.log(character.direction)
+  if (character.direction === 'up') {
+    character.className = 'rotate270'
+    character.style.top = characterY - ghostSpeed + 'px'
+  }
+  if (character.direction === 'down') {
+    character.className = 'rotate90'
+    character.style.top = characterY + ghostSpeed + 'px'
+  }
+  if (character.direction === 'left') {
+    character.className = 'mirrorImage'
+    if(characterX < 0) characterX = 560
+    character.style.left = characterX - ghostSpeed + 'px'
+  }
+  if (character.direction === 'right') {
+    character.className = ''
+    if(characterX > 560) characterX = 0
+    character.style.left = characterX + ghostSpeed + 'px'
+  }
+  if (hitWall(character) || hitGhost(character)) {
+    character.style.left = charOriginalLeft
+    character.style.top = charOriginalTop
+    ghostHitWallMovement(character)
+  }
+}
+
+function ghostHitWallMovement (character) {
+  var oppDirection
+  if (character.direction === 'up') oppDirection = 'down'
+  if (character.direction === 'down') oppDirection = 'up'
+  if (character.direction === 'left') oppDirection = 'right'
+  if (character.direction === 'right') oppDirection = 'left'
+
+  var x = false
+  while (!x) {
+    var newDirection = randomGhostDirection()
+    if (newDirection !== oppDirection && newDirection !== character.direction) {
+      character.direction = newDirection
+      x = true
+    }
+  }
+ghostMovement(character)
+}
+
+// random ghost direction
+function randomGhostDirection () {
+  var ghostDirection = ['up', 'down', 'left', 'right']
+  var direction = ghostDirection[Math.floor(Math.random() * (3 - 0 + 1) + 0)]
+  return direction
 }
 
 // game loop
 function loop () {
   numLoops++
   pacmanMovement()
-  blueGhostMovement ()
+  ghostMovement(blueGhost)
+  ghostMovement(orangeGhost)
+  ghostMovement(pinkGhost)
+  ghostMovement(redGhost)
 }
