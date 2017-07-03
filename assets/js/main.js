59c8e23fbc194f5d38d32a1ca5423f47036d6032
Dominic Phua <dominikphua@gmail.com>
2017-07-04 00:33:02 +0800

    Functioning game!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,40 @@
 gameArray = []
 console.log(gameArray)
-// var value = 0
+var counter = 0
+var numpad = 0
 
+// ---------------------------------------------------------
 function randomizer (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
-
+// ---------------------------------------------------------
 function gameStart () {
   gameArray.push(randomizer(1, 4), randomizer(1, 4), randomizer(1, 4), randomizer(1, 4))
 }
-
-//Start button of the game.
-gameStart()
-console.log(gameArray)
-
+// ---------------------------------------------------------
 function turn () {
   gameArray.pop()
   gameArray.unshift(randomizer(1, 4))
-  console.log(gameArray)
+  // console.log(gameArray)
 }
-
+// ---------------------------------------------------------
 function tiles () {
-  var allSquare = document.querySelectorAll(".square")
+  var allSquare = document.querySelectorAll('.square')
   for (var i = 0; i < allSquare.length; i++) {
     allSquare[i].style.background = 'white'
   }
   turn()
-  document.querySelector("#square" + gameArray[0]).style.background = 'black'
+  document.querySelector('#square' + gameArray[0]).style.background = 'black'
   var squareTwo = gameArray[1] + 4
   var squareThree = gameArray[2] + 8
   var squareFour = gameArray[2] + 12
-  document.querySelector("#square" + squareTwo).style.background = 'black'
-  document.querySelector("#square" + squareThree).style.background = 'black'
-  document.querySelector("#square" + squareFour).style.background = 'grey'
+  document.querySelector('#square' + squareTwo).style.background = 'black'
+  document.querySelector('#square' + squareThree).style.background = 'black'
+  document.querySelector('#square' + squareFour).style.background = 'grey'
 }
-
-//------------------------------------------------------------------
-
-document.addEventListener ('click', tiles)
-
-//------------------------------------------------------------------
-document.addEventListener('keydown', buttons)
-
+// ---------------------------------------------------------
 function buttons (event) {
-  var numpad = 0
+  // var numpad = 0
   if (event.keyCode === 90) {
     numpad = 9
   } else if (event.keyCode === 88) {
@@ -53,5 +44,38 @@ function buttons (event) {
   } else if (event.keyCode === 77) {
     numpad = 12
   }
-  console.log(numpad)
+  // console.log(numpad)
 }
+// ---------------------------------------------------------
+gameStart()
+console.log(gameArray)
+document.querySelector('.counter').innerHTML = '0'
+
+document.addEventListener('click', tiles)
+// console.log(gameArray)
+// document.addEventListener('keydown', buttons)
+// ---------------------------------------------------------
+function restart () {
+  gameArray = []
+  counter = 0
+  var allSquare = document.querySelectorAll('.square')
+  for (var i = 0; i < allSquare.length; i++) {
+    allSquare[i].style.background = 'white'
+  }
+}
+// ---------------------------------------------------------
+document.addEventListener('keydown', function () {
+  var squareThree = gameArray[2] + 8
+  buttons(event)
+  if (numpad == squareThree) {
+    tiles()
+    counter++
+    document.querySelector('.counter').innerHTML = counter
+  } else {
+    alert('Game Over')
+    restart()
+    gameStart()
+    document.querySelector('.counter').innerHTML = '0'
+  }
+})
+// ---------------------------------------------------------
