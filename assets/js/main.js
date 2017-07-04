7b5813054a70586cbc865ccd3737d4da1d8b12c6
Daniel Lim <ddanielnp@hotmail.com>
2017-07-04 11:38:33 +0800

    major MODs to DOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
-document.addEventListener('DOMContentLoaded', init)
+// document.addEventListener('DOMContentLoaded', init)
 
-function init () {
+// var bs = burgerStack()
+
+// function init () {
+// userinput hamburger
   var u1 = document.getElementById('u1')
   var u2 = document.getElementById('u2')
   var u3 = document.getElementById('u3')
@@ -9,14 +12,23 @@ function init () {
   var u6 = document.getElementById('u6')
   var u7 = document.getElementById('u7')
   var hamItem = document.querySelectorAll('.hamItem')
-  // var index = hamItem.length -1
-  console.log(index);
-  // console.log(hamItem[hamItem.length - 1]);
-  // console.log(hamItem.length - 1);
-
+// generated hamburger
+  var d1 = document.getElementById('d1')
+  var d2 = document.getElementById('d2')
+  var d3 = document.getElementById('d3')
+  var d4 = document.getElementById('d4')
+  var d5 = document.getElementById('d5')
+  var d6 = document.getElementById('d6')
+  var d7 = document.getElementById('d7')
+  var hamObj = document.querySelectorAll('.hamObj')
+  var gameStart = true
+  var counter = 0
+  var start = document.querySelector('.start')
+  var reset = document.querySelector('.reset')
 
   document.addEventListener('keyup', onKeyUp)
-var index = 4
+  var index = 4
+  // player chooses ingredients to the userinput screen
   function onKeyUp (event) {
     if (checkKeyCode(event.keyCode)) {
       if (event.keyCode === 81) {
@@ -47,15 +59,24 @@ var index = 4
         index--
       }
       if (event.keyCode === 32) {
-        document.getElementById('u1').classList.add('topbun')
-        setTimeout(function(){resetClass()},500);
+        u1.classList.add('topbun')
+        if (checkIngredient) {
+          setTimeout(function () {
+            resetClassPlayer()
+            resetClassDisplay()
+          }, 800)
+          gamePlay()
+        } else {
+          resetClassPlayer()
+          resetClassDisplay()
+          // gamePlay()
+        }
+        alert('wrong button pressed')
       }
-    } else {
-      alert('wrong button pressed')
     }
   }// closing for onKeyUp
 
-  function resetClass () {
+  function resetClassPlayer () {
     u1.classList.remove('topbun')
     u2.classList = 'hamItem'
     u3.classList = 'hamItem'
@@ -65,22 +86,113 @@ var index = 4
     index = 4
   }
 
-  // for generating hamburger display
-  var d1 = document.getElementById('d1')
-  var d2 = document.getElementById('d2')
-  var d3 = document.getElementById('d3')
-  var d4 = document.getElementById('d4')
-  var d5 = document.getElementById('d5')
-  var d6 = document.getElementById('d6')
-  var d7 = document.getElementById('d7')
+  // for generating the hamburger display
+  var count = 4
+  function randomizeIngredient () {
+    var creation = Math.floor(Math.random() * 5)
+  // console.log(creation)
+    if (creation === 0) {
+      hamObj[count].classList.add('cucumber')
+      hamObj[count].classList.remove('hamObj')
+      count--
+    // return 'cucumber'
+    }
+    if (creation === 1) {
+      hamObj[count].classList.add('meat')
+      hamObj[count].classList.remove('hamObj')
+      count--
+    // return 'meat'
+    }
+    if (creation === 2) {
+      hamObj[count].classList.add('onion')
+      hamObj[count].classList.remove('hamObj')
+      count--
+    // return 'onion'
+    }
+    if (creation === 3) {
+      hamObj[count].classList.add('lettuce')
+      hamObj[count].classList.remove('hamObj')
+      count--
+    // return 'lettuce'
+    }
+    if (creation === 4) {
+      hamObj[count].classList.add('cheese')
+      hamObj[count].classList.remove('hamObj')
+      count--
+    // return 'cheese'
+    }
+  }
 
+  function resetClassDisplay () {
+    d1.classList.remove('topbun')
+    d2.classList = 'hamObj'
+    d3.classList = 'hamObj'
+    d4.classList = 'hamObj'
+    d5.classList = 'hamObj'
+    d6.classList = 'hamObj'
+    count = 4
+  }
 
+  // Burger levels creation
+  // 2 random ingredients
+  function level1 () {
+    for (var i = 0; i < 2; i++) {
+      randomizeIngredient()
+    } return true
+  }
+  // 3 random ingredients
+  function level2 () {
+    for (var i = 0; i < 3; i++) {
+      randomizeIngredient()
+    } return true
+  }
+  // 4 random ingredients
+  function level3 () {
+    for (var i = 0; i < 4; i++) {
+      randomizeIngredient()
+    } return true
+  }
+  // 5 random ingredients
+  function level4 () {
+    for (var i = 0; i < 5; i++) {
+      randomizeIngredient()
+    } return true
+  }
+  // end of Burger levels creation
+
+    // function for startgame
+  function gamePlay () {
+    if (gameStart === true) {
+      if (counter <= 3) {
+        level1()
+      }
+      if (counter > 3 && counter <= 7) {
+        level2()
+      }
+      if (counter > 7 && counter <= 11) {
+        level3()
+      }
+      if (counter > 11) {
+        level4()
+      }
+      counter++
+    } else gameStart = false
+  } // end of startgame
+
+  function checkIngredient () {
+    for (var i = 7; i > 2; i--) {
+      if ('d[i]'.classList === 'u[i]'.classList) {
+        return true
+      }
+    }
+  }
+
+  start.addEventListener('click', level1)
 
   function checkKeyCode (keycode) {
     if (keycode === 81 || keycode === 87 || keycode === 69 || keycode === 82 || keycode === 84 || keycode === 32) {
       return true
     }
     return false
-  }//closing for checkKeyCode
-
-}// closing for init
+  }// closing for checkKeyCode
+// }// closing for init
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,152 +1,154 @@
-var player = true
-var gameStart = true
-var match = false
-var counter = 0
-var gameOver = false
-var score = {
-  cucumber: 10,
-  meat: 10,
-  onion: 10,
-  lettuce: 10,
-  cheese: 10
-}
-var randomArray = [] // random food array
-var playerFoodInput = [] // player chosen food array
-var displayFoodInput = [] // generated food array
-// var chosenIngredient = [] // chosen ingredient array
-var timer = 0
-
-// function for check if gameover
-function isGameOver () {
-
-}
-
-// function for startgame
-function gamePlay () {
-  if (gameStart === true) {
-    if (counter <= 3) {
-      level1()
-    }
-    if (counter > 3 && counter <= 7) {
-      level2()
-    }
-    if (counter > 7 && counter <= 11) {
-      level3()
-    }
-    if (counter > 11) {
-      level4()
-    }
-    counter++
-  } else gameStart = false
-} // closing for gamePlay
-
-// function to create randomIngredient
-function randomizeIngredient () {
-  var ingredient = Math.floor(Math.random() * 5)
-  if (ingredient === 0) {
-    randomArray.push('cucumber')
-    return 'cucumber'
-  }
-  if (ingredient === 1) {
-    randomArray.push('meat')
-    return 'meat'
-  }
-  if (ingredient === 2) {
-    randomArray.push('onion')
-    return 'onion'
-  }
-  if (ingredient === 3) {
-    randomArray.push('lettuce')
-    return 'lettuce'
-  }
-  if (ingredient === 4) {
-    randomArray.push('cheese')
-    return 'cheese'
-  }
-}
-
-function chooseIngredient (food) {
-  if (food == 'cucumber') {
-    playerFoodInput.push('cucumber')
-    return true
-  }
-  if (food == 'meat') {
-    playerFoodInput.push('meat')
-    return true
-  }
-  if (food == 'onion') {
-    playerFoodInput.push('food')
-    return true
-  }
-  if (food == 'lettuce') {
-    playerFoodInput.push('lettuce')
-    return true
-  }
-  if (food == 'cheese') {
-    playerFoodInput.push('cheese')
-    return true
-  }
-}// closing for chooseIngredient
-
-// 2 random ingredients
-function level1 () {
-  for (var i = 0; i < 2; i++) {
-    randomizeIngredient()
-    console.log(randomArray)
-  } return true
-}
-// 3 random ingredients
-function level2 () {
-  for (var i = 0; i < 3; i++) {
-    randomizeIngredient()
-  } return true
-}
-// 4 random ingredients
-function level3 () {
-  for (var i = 0; i < 4; i++) {
-    randomizeIngredient()
-  } return true
-}
-// 5 random ingredients
-function level4 () {
-  for (var i = 0; i < 5; i++) {
-    randomizeIngredient()
-  } return true
-}
-
+// var player = true
+// var gameStart = true
+// var match = false
+// var counter = 0
+// var gameOver = false
+// var score = {
+//   cucumber: 10,
+//   meat: 10,
+//   onion: 10,
+//   lettuce: 10,
+//   cheese: 10
+// }
+// var randomArray = [] // random food array
+// var playerFoodInput = [] // player chosen food array
+// var displayFoodInput = [] // generated food array
+// // var chosenIngredient = [] // chosen ingredient array
+// var timer = 0
+//
+// // function burgerStack () {
+//
+  // function for startgame
+  // function gamePlay () {
+  //   if (gameStart === true) {
+  //     if (counter <= 3) {
+  //       level1()
+  //     }
+  //     if (counter > 3 && counter <= 7) {
+  //       level2()
+  //     }
+  //     if (counter > 7 && counter <= 11) {
+  //       level3()
+  //     }
+  //     if (counter > 11) {
+  //       level4()
+  //     }
+  //     counter++
+  //   } else gameStart = false
+  // } // closing for gamePlay
+//
+// // function to create randomIngredient
+//   function randomizeIngredient () {
+//     var ingredient = Math.floor(Math.random() * 5)
+//     if (ingredient === 0) {
+//       randomArray.push('cucumber')
+//       return 'cucumber'
+//     }
+//     if (ingredient === 1) {
+//       randomArray.push('meat')
+//       return 'meat'
+//     }
+//     if (ingredient === 2) {
+//       randomArray.push('onion')
+//       return 'onion'
+//     }
+//     if (ingredient === 3) {
+//       randomArray.push('lettuce')
+//       return 'lettuce'
+//     }
+//     if (ingredient === 4) {
+//       randomArray.push('cheese')
+//       return 'cheese'
+//     }
+//   } // closing for randomizeIngredient
+//
+//   function chooseIngredient (food) {
+//     if (food == 'cucumber') {
+//       playerFoodInput.push('cucumber')
+//       return 'cucumber'
+//     }
+//     if (food == 'meat') {
+//       playerFoodInput.push('meat')
+//       return 'meat'
+//     }
+//     if (food == 'onion') {
+//       playerFoodInput.push('onion')
+//       return 'onion'
+//     }
+//     if (food == 'lettuce') {
+//       playerFoodInput.push('lettuce')
+//       return 'lettuce'
+//     }
+//     if (food == 'cheese') {
+//       playerFoodInput.push('cheese')
+//       return 'cheese'
+//     }
+//   }// closing for chooseIngredient
+//
+// // 2 random ingredients
+//   function level1 () {
+//     for (var i = 0; i < 2; i++) {
+//       randomizeIngredient()
+//       console.log(randomArray)
+//     } return true
+//   }
+// // 3 random ingredients
+//   function level2 () {
+//     for (var i = 0; i < 3; i++) {
+//       randomizeIngredient()
+//     } return true
+//   }
+// // 4 random ingredients
+//   function level3 () {
+//     for (var i = 0; i < 4; i++) {
+//       randomizeIngredient()
+//     } return true
+//   }
+// // 5 random ingredients
+//   function level4 () {
+//     for (var i = 0; i < 5; i++) {
+//       randomizeIngredient()
+//     } return true
+//   }
+//
 // function for checking if ingredient picked is same as displayed burger
-function checkIngredient () {
+  function checkIngredient () {
   // var playerInput = playerFoodInput.join('')
   // var randomInput = randomArray.join('')
-  for (var i = 0; i < playerFoodInput.length; i++) {
-    if (randomArray[i] == playerFoodInput[i]) {
-      return true
-    } return false
+    for (var i = 0; i < playerFoodInput.length; i++) {
+      if (randomArray[i] == playerFoodInput[i]) {
+        return gamePlay()
+      } return wrongPress()
+    }
   }
-}
-
-// function for wrong button pressed
-function wrongPress () {
-  if (checkIngredient !== true) {
-    resetGame ()
-  }
-}
-
 //
+// // function for wrong button pressed
+//   function wrongPress () {
+//     if (checkIngredient !== true) {
+//       resetGame()
+//     }
+//   }
 //
-// function for timer
-function timer () {
-
-}
-
-// function for checking timer
-function checkTimer () {
-
-}
-
-// function for restarting game
-function resetGame () {
-  var randomArray = []
-  var playerFoodInput = []
-  var displayFoodInput = []
-}
+// // function for check if gameover
+//   function isGameOver () {
+//
+//   }
+// //
+// //
+// // function for timer
+//   function timer () {
+//
+//   }
+//
+// // function for checking timer
+//   function checkTimer () {
+//
+//   }
+//
+// // function for restarting game
+//   function resetGame () {
+//     var randomArray = []
+//     var playerFoodInput = []
+//     var displayFoodInput = []
+//   }
+// // }
