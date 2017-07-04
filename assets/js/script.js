37c70567762a62bc8205114189fe9aabf61ce35f
Shaun Loh <loh.shaun89@gmail.com>
2017-07-04 14:09:29 +0800

    created changing colors function with js for every level

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,18 @@ document.addEventListener('DOMContentLoaded', function () {
   var tile
   var score = 0
   var timeLeft = 30
+  var palette = [
+    ['rgb(20, 153, 105)',
+      'rgb(25, 168, 131)'],
+    ['rgb(235, 152, 20)',
+      'rgb(220, 160, 40)'],
+    ['rgb(0, 117, 255)',
+      'rgb(30, 117, 220)'],
+    ['rgb(255, 237, 0)',
+      'rgb(255, 230, 40)']
+  ]
+
+  console.log(palette[3][0])
 
 // initialize the game
   function init () {
@@ -52,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // game timer
   function gameTimer () {
-// target timer node
+    // target timer node
     var timer = document.querySelector('.timer')
     var timerId = setInterval(countdown, 1000)
 
@@ -80,6 +92,21 @@ document.addEventListener('DOMContentLoaded', function () {
       }
       document.querySelector('.container').innerHTML += '</div>'
     }
+    // grid color
+    // '.L' + noOfRows + 'Grid'
+    var allGrids = document.querySelectorAll('.L' + noOfRows + 'Grid')
+    for (var p = 0; p < noOfRows-1; p++){
+      for (var j = 0; j < gridId; j++)
+      allGrids[j].style.backgroundColor = palette[p][0]
+      // randomize one tile to make odd
+    }
+  }
+
+// random number btw max & min
+  function ranNum (min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
   }
 
 // randomize a tile position
@@ -99,10 +126,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // target the start node
   var startButtonTgt = document.querySelector('.startButton')
-
 // add event listener to start button
   startButtonTgt.addEventListener('click', function () {
-  // init function
     init()
   })
 
@@ -114,25 +139,28 @@ document.addEventListener('DOMContentLoaded', function () {
   wholeGrid.forEach(function (elem) {
     elem.addEventListener('click', function () {
       // empty out previous grid squares
-      document.querySelector('.container').innerHTML = " "
-      // increase the grid size
-      noOfRows += 1
-      console.log(noOfRows)
+      document.querySelector('.container').innerHTML = ' '
+      // increase the grid size if < 5x5
+      if (noOfRows < 5) {
+        noOfRows += 1
+        console.log(noOfRows)
+      }
       // create the new grid
       gridCreate()
+
       // if (elem.id === tile.id) {
       //   elem.style.backgroundColor = 'red'
         // ranPosition()
+
         // score += 1
-        // console.log(score)
       // target the score node
-        var scoreTgt = document.querySelector('.score')
+      var scoreTgt = document.querySelector('.score')
       // update score to score node
-        scoreTgt.innerHTML = score
+      scoreTgt.innerHTML = score
+      // console.log(score)
       // }
     })
   })
 })
-// delay timer for color to appear
 
 // check if correct grid is clicked
