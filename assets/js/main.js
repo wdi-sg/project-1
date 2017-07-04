e4df1ed2ab5c22a4f598183f120c63b25e184a06
Rebecca Tay <rebeccatay92@gmail.com>
2017-07-04 14:56:39 +0800

    minor font size change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var ingredientCounter = 0 // number of ingredients already in play
-var level = 1 //determines score and number of things in order list
+var level = 1 // determines score and number of things in order list
 var playArea = document.querySelector('.playArea')
 var buttons = document.querySelectorAll('.buttons div')
 var serveButton = document.querySelector('.serveButton')
@@ -25,19 +25,19 @@ buttons.forEach(function (el) {
 function addIngredient () {
   if (gameStarted) {
    // whichIngredient takes out the classnames 'cheese', 'patty' etc.
-   whichIngredient = this.className.substring(5).toLowerCase()
-   if (whichIngredient === neededIngredients[ingredientCounter]) {
-    var newIngredient = document.createElement('div')
+    whichIngredient = this.className.substring(5).toLowerCase()
+    if (whichIngredient === neededIngredients[ingredientCounter]) {
+      var newIngredient = document.createElement('div')
      // give the new element classname ingredients, which sets core css
-    newIngredient.className = 'ingredients'
+      newIngredient.className = 'ingredients'
      // id sets background-image url and negative margins
-    newIngredient.id = whichIngredient
-    newIngredient.style.bottom = (60 + ingredientCounter * 20) + "px"
+      newIngredient.id = whichIngredient
+      newIngredient.style.bottom = (60 + ingredientCounter * 20) + 'px'
      // increasing z-index for overlapping look
-    ingredientCounter++ // counts num of added ingredients so far
-    newIngredient.style.zIndex = ingredientCounter
-    playArea.prepend(newIngredient)
-  }
+      ingredientCounter++ // counts num of added ingredients so far
+      newIngredient.style.zIndex = ingredientCounter
+      playArea.prepend(newIngredient)
+    }
   }
 }
 
@@ -61,7 +61,7 @@ function generateList () {
   var orderList = newOrder()
   orderList.forEach(function (el) {
     var newListItem = document.createElement('h3')
-    if (el !== "topbun") {
+    if (el !== 'topbun') {
       newListItem.innerText = el
     } else newListItem.innerText = 'top bun'
     order.appendChild(newListItem)
@@ -69,21 +69,21 @@ function generateList () {
 }
 
 function clearList () {
-  order.innerHTML = ""
-  neededIngredients = [] //reset the array for clicks to compare against
+  order.innerHTML = ''
+  neededIngredients = [] // reset the array for clicks to compare against
 }
 
 // serve button clears playArea, clears list, increases score, generates new orderList, reset ingredientcounter to 0.
 serveButton.addEventListener('click', serve)
 
 function serve () {
-  if (gameStarted && ingredientCounter === level + 5) { //4 + 1 to account for bottombun
+  if (gameStarted && ingredientCounter === level + 5) { // 4 + 1 to account for bottombun
     clearPlayArea()
     increaseScore()
     clearList()
     generateList()
     ingredientCounter = 0
-    //reset current count to zero since ingredients have been cleared off playArea
+    // reset current count to zero since ingredients have been cleared off playArea
   }
 }
 
@@ -106,7 +106,7 @@ function increaseScore () {
 var timeLeft = 90
 startGameButton.addEventListener('click', startGame)
 
-function startGame() {
+function startGame () {
   if (!gameStarted) { // ensure event only fires once.
     setInterval(countdown, 1000)
     generateList()
@@ -114,7 +114,7 @@ function startGame() {
   }
    // click to start generates first order
   gameStarted = true
-  startGameOverlay.style.height = "0px"
+  startGameOverlay.style.height = '0px'
 }
 
 // callback to change timeLeft and update DOM text
@@ -123,14 +123,13 @@ function countdown () {
   if (timeLeft >= 0) {
     time.innerText = timeLeft + ' secs'
   }
-
 }
 
-function gameOver() {
-  endGameOverlay.style.height = "768px"
+function gameOver () {
+  endGameOverlay.style.height = '768px'
   endGameScore.innerText = score.innerText
 }
 
-restart.addEventListener('click', function() {
+restart.addEventListener('click', function () {
   window.location.reload()
 })
