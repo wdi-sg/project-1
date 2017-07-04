bc0f4dd929bac5977e8885b56877d3d96742d1b7
mmmlll <michelle.y.lai@gmail.com>
2017-07-04 14:37:00 +0800

    updated game.html and results.html

--- scripts/callprice.js
+++ scripts/callprice.js
@@ -1,59 +1,6 @@
-// Modified from: https://stackoverflow.com/questions/12460378/how-to-get-json-from-url-in-javascript
-// That example uses browser standard, not node. If node, need to install npm xmlhttprequest, and "require" at top of code
+// Modified from: Eloquent JS. Works for quandl, not for google finance
 
 var req = new XMLHttpRequest()
-req.open("GET", "https://www.quandl.com/api/v3/datasets/HKEX/02318.json?api_key=1VqFiTXxySMhKgkyNfPp", false)
+req.open('GET', 'https://www.quandl.com/api/v3/datasets/HKEX/02318.json?2&api_key=1VqFiTXxySMhKgkyNfPp', false)
 req.send(null)
 console.log(JSON.parse(req.responseText))
-
-// THIS SECTION BELOW IS FOR THE GOOGLE FINANCE SOURCE
-
-// var getJSON = function(url, callback) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('GET', url, true);
-//     xhr.responseType = 'json';
-//     xhr.onload = function() {
-//       var status = xhr.status;
-//       if (status == 200) {
-//         callback(null, xhr.response);
-//       } else {
-//         callback(status);
-//       }
-//     };
-//     xhr.send(null);
-// };
-//
-// getJSON('https://finance.google.com/finance/info?client=ig&q=HKG%3A2318',
-// function(err, data) {
-//   if (err != null) {
-//     alert('Something went wrong: ' + err);
-//   } else {
-//     alert('Your query count: ' + data.query.count);
-//   }
-// });
-
-// THIS SECTION BELOW IS FOR THE QUANDL SOURCE
-//
-// var getJSON = function(url, callback) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('GET', url, true);
-//     xhr.responseType = 'json';
-//     xhr.onload = function() {
-//       var status = xhr.status;
-//       if (status == 200) {
-//         callback(null, xhr.response);
-//       } else {
-//         callback(status);
-//       }
-//     };
-//     xhr.send(null);
-// };
-//
-// getJSON('https://www.quandl.com/api/v3/datasets/HKEX/02318.json?api_key=1VqFiTXxySMhKgkyNfPp',
-// function(err, data) {
-//   if (err != null) {
-//     alert('Something went wrong: ' + err);
-//   } else {
-//     alert('Your query count: ' + data.query.count);
-//   }
-// });
---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+// Loads with index.html file. Not sure what to load here - maybe an init file? 
