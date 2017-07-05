7da4792f1a33ba70b0ae5e8696814e10a220e735
Ernest Tan <evilernie1985@gmail.com>
2017-07-05 11:04:23 +0800

    Gameover added; restart fn in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,15 +38,29 @@ function init () {
 
   // all fns
   function showQuestionAndChoices (qIndex) {
-    questionDisplay.textContent = allQuestions[qIndex].question
-    ansDisplay.innerHTML = ''
 
-    for (var i = 0; i < allQuestions[qIndex].choices.length; i++) {
-      var newLi = document.createElement('li')
-      newLi.textContent = allQuestions[qIndex].choices[i]
-      ansDisplay.appendChild(newLi)
+
+    if (qIndex <= 3) {
+      questionDisplay.textContent = allQuestions[qIndex].question
+      ansDisplay.innerHTML = ''
+
+      for (var i = 0; i < allQuestions[qIndex].choices.length; i++) {
+        var newLi = document.createElement('li')
+        newLi.textContent = allQuestions[qIndex].choices[i]
+        ansDisplay.appendChild(newLi)
+      }
+    } else {
+
+      if (player1Score > player2Score) {
+        alert('Game Over, Player 1 Wins!')
+      } else if (player2Score > player1Score) {
+        alert('Game Over, PLayer 2 Wins!')
+      } else if (player1Score === player2Score) {
+        alert('its a tie!')
+      }
     }
   }
+
 
   function turnKeeper (questionNum) {
     // console.log(qIndex);
@@ -66,7 +80,8 @@ function init () {
     // console.log(currentPlayer)
     // console.log('check answer')
     // console.log('current qn', currentQn)
-    console.log('', currentQn.choices[currentQn.correctAns])
+    // console.log('', currentQn.choices[currentQn.correctAns])
+
     if (currentPlayer === 1 && event.target.textContent === currentQn.choices[currentQn.correctAns]) {
       player1Score += 1
       p1ScoreDisplay.textContent = 'Player 1\'s score is' + ' ' + player1Score
@@ -77,23 +92,7 @@ function init () {
         p2ScoreDisplay.textContent = 'Player 2\'s score is' + ' ' + player2Score
         console.log(player2Score)
       }
-
     }
-
-  //   for (var i = 0; i < allList.length; i++) {
-  //     var addAnsListen = allList[i]
-  // //
-  //     console.log(allList[i])
-  //     addAnsListen.addEventListener('click', function (event) {
-  //       console.log(event.target.textContent)
-  //       console.log(currentQn.choices[currentQn.correctAns])
-  // //
-  //
-  // //
-  //
-  // //       // update div id player1Score to reflect change
-  //       }
-  //     }
   }
 
   function activateChoices () {
@@ -110,106 +109,15 @@ function init () {
       })
     }
   }
+  function restart() {
+
+  }
+
+    // To do list:
+    // Create function to check when game ends + determine who wins
+    // Create reset button to restart game
 
   // start at qn 1
   showQuestionAndChoices(questionNum)
   activateChoices()
-
-  // for (var i = 0; i < allList.length; i++) {
-  // input: q index
-  // output: nothing
-  // side effect: write questions and choices
-
-    // function showQuestionAndChoices (qIndex) {
-      // questionDisplay.textContent = allQuestions[qIndex].question
-      // ansDisplay.innerHTML = ''
-
-    // console.log(ansDisplay);
-
-      // for (var i = 0; i < allQuestions[qIndex].choices.length; i++) {
-      //   var newLi = document.createElement('li')
-      //   newLi.textContent = allQuestions[qIndex].choices[i]
-      //   ansDisplay.appendChild(newLi)
-      // }
-    // console.log(allQuestions[qIndex].question)
-    // display question asked & q choices
-    // }
-
-//     function choicesClicker () {
-//       for (var i = 0; i < allList.length; i++) {
-//         var addAnsListen = allList[i]
-//
-//         console.log(allList[i])
-//         addAnsListen.addEventListener('click', function (event) {
-//           console.log(event.target.textContent)
-//           console.log(currentQn.choices[currentQn.correctAns])
-//
-//           if (event.target.textContent === currentQn.choices[currentQn.correctAns]) {
-//             var p1ScoreDisplay = document.querySelector('#player1Score')
-//
-//             player1Score += 1
-//             p1ScoreDisplay.textContent = player1Score
-//             console.log(player1Score)
-//           // update div id player1Score to reflect change
-//           } else {
-//             console.log('wrong, do sth else, change next qn')
-//           }
-//         })
-//
-// // Add more questions to quiz
-// // Program switching between P1 and P2
-// // Ensure scoring works for P1 and P2
-//
-//         questionNum++
-//         showQuestionAndChoices(questionNum)
-//
-//
-//       // change to q2
-//       }
-//     }
-
-  // function correctOrNot (ans) {
-  //   if (questionOneAns() == allQuestions[0].choices[0]) {
-  //     console.log('Correct!')
-  //   }
-  //
-  //   // get a string
-  //   // compare with choices[]
-  //   // if string ===
 }
-
-  //   console.log(allLis);
-
-  // newLi.addEventListener('click', function (event) {
-
-//   addAnsListen.addEventListener('click', function (event) {
-//   console.log(allLis);
-// })
-
-//   newLi.addEventListener('click', function (event) {
-//     var clickedLi = event.target
-//     console.log(clickedLi.textContent)
-// }
-
-  //
-  // })
-
-  // how to target textContent that was being clicked
-
-  // var newLi = document.createElement('li')
-  // nexLi.textContent = questionOne.choices[0]
-  // ansDisplay.appendChild(newLi)
-  //
-
-  // var newLi = document.createElement('li')
-  // newLi.textContent = questionOne.choices[1]
-  // ansDisplay.appendChild(newLi)
-  //
-  //
-  // var newLi = document.createElement('li')
-  // newLi.textContent = questionOne.choices[2]
-  // ansDisplay.appendChild(newLi)
-  //
-  // var newLi = document.createElement('li')
-  // newLi.textContent = questionOne.choices[3]
-  // ansDisplay.appendChild(newLi)
