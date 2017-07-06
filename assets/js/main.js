8cffb76bac0b7c45f38fef41697589c5c1abc648
Dominic Phua <dominikphua@gmail.com>
2017-07-06 09:37:39 +0800

    Updated visuals

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,10 @@ var gameArray = []
 var counter = 0
 var numpad = 0
 var startpad = 0
-var time = 10
+var time = 30
 
 document.querySelector('.counter').innerHTML = '0'
-document.querySelector('.time').innerHTML = '10'
+document.querySelector('.time').innerHTML = '30'
 
 // ---------------------------------------------------------
 function randomizer (min, max) {
@@ -27,22 +27,47 @@ function tiles () {
     allSquare[i].style.background = 'white'
   }
   turn()
-  document.querySelector('#square' + gameArray[0]).style.background = 'black'
+  // document.querySelector('#square' + gameArray[0]).style.background = 'purple'
   var squareTwo = gameArray[1] + 4
   var squareThree = gameArray[2] + 8
   var squareFour = gameArray[2] + 12
-  document.querySelector('#square' + squareTwo).style.background = 'black'
-  document.querySelector('#square' + squareThree).style.background = 'black'
-  document.querySelector('#square' + squareFour).style.background = 'grey'
+  // document.querySelector('#square' + squareTwo).style.background = 'black'
+  // document.querySelector('#square' + squareThree).style.background = 'black'
+  // document.querySelector('#square' + squareFour).style.background = 'grey'
 
-  // var test = document.querySelector('#square' + squareFour)
-  // test.style.backgroundImage = 'url("https://pbs.twimg.com/profile_images/604644048/sign051.gif")'
-  // test.style.backgroundSize = '100%'
-  // test.style.backgroundRepeat = 'no-repeat'
-  // test.style.verticalAlign = 'middle'
-  // test.style.width = '90px'
-  // test.style.height = 'auto'
-  // document.querySelector('#square' + squareFour).style.backgroundImage = 'url("https://pbs.twimg.com/profile_images/604644048/sign051.gif")'
+  // var squareOneStyle = document.querySelector('#square' + squareOne)
+  // squareOneStyle.style.backgroundImage = 'url("http://orig04.deviantart.net/7d15/f/2016/366/c/0/cat_head_png__by_madcatmd-datnw9e.png")'
+  // squareOneStyle.style.backgroundSize = '100%'
+  // squareOneStyle.style.backgroundRepeat = 'no-repeat'
+  // squareOneStyle.style.backgroundPosition = 'center'
+
+  var squareOneStyle = document.querySelector('#square' + gameArray[0])
+  squareOneStyle.style.backgroundImage = 'url("http://orig04.deviantart.net/7d15/f/2016/366/c/0/cat_head_png__by_madcatmd-datnw9e.png")'
+  squareOneStyle.style.backgroundSize = '100%'
+  squareOneStyle.style.backgroundRepeat = 'no-repeat'
+  squareOneStyle.style.backgroundPosition = 'center'
+
+
+  var squareTwoStyle = document.querySelector('#square' + squareTwo)
+  squareTwoStyle.style.backgroundImage = 'url("http://orig04.deviantart.net/7d15/f/2016/366/c/0/cat_head_png__by_madcatmd-datnw9e.png")'
+  squareTwoStyle.style.backgroundSize = '100%'
+  squareTwoStyle.style.backgroundRepeat = 'no-repeat'
+  squareTwoStyle.style.backgroundPosition = 'center'
+
+
+
+  var squareThreeStyle = document.querySelector('#square' + squareThree)
+  squareThreeStyle.style.backgroundImage = 'url("http://orig04.deviantart.net/7d15/f/2016/366/c/0/cat_head_png__by_madcatmd-datnw9e.png")'
+  squareThreeStyle.style.backgroundSize = '100%'
+  squareThreeStyle.style.backgroundRepeat = 'no-repeat'
+  squareThreeStyle.style.backgroundPosition = 'center'
+
+
+  var squareFourStyle = document.querySelector('#square' + squareFour)
+  squareFourStyle.style.backgroundImage = 'url("http://orig04.deviantart.net/7d15/f/2016/366/c/0/cat_head_png__by_madcatmd-datnw9e.png")'
+  squareFourStyle.style.backgroundSize = '100%'
+  squareFourStyle.style.backgroundRepeat = 'no-repeat'
+  squareFourStyle.style.backgroundPosition = 'center'
 }
 // ---------------------------------------------------------
 function buttons (event) {
@@ -63,11 +88,13 @@ function buttons (event) {
 function restart () {
   gameArray = []
   counter = 0
-  time = 10
+  time = 30
   fired = false
   var allSquare = document.querySelectorAll('.square')
   for (var i = 0; i < allSquare.length; i++) {
     allSquare[i].style.background = 'white'
+  document.querySelector('.counter').innerHTML = '0'
+  document.querySelector('.time').innerHTML = '30'
   }
 }
 // ---------------------------------------------------------
@@ -78,16 +105,19 @@ function timer () {
     time = time - 1
     // console.log(time)
     document.querySelector('.time').innerHTML = time
-  } else if (time = 0) {
+  } else if (time === 0) {
     alert('Time Over')
     restart()
-    clearInterval(interval)
+    clearInterval(test)
+    tiles()
   }
 }
 
+var test = ''
+
 function interval () {
   if (fired = true) {
-    setInterval(timer, 1000)
+    test = setInterval(timer, 1000)
   }
 }
 
@@ -96,70 +126,48 @@ function intervalTimer () {
     fired = true
     interval()
     gameStart()
-      // tiles()
+    tiles()
     }
   }
-// ---------------------------------------------------------
-// gameStart()
 console.log(gameArray)
 // ---------------------------------------------------------
-// document.addEventListener('click', function () {
-//   tiles()
-//   var interval = setInterval(timer, 1000)
-//   function timer () {
-//     if (time > 0) {
-//       time = time - 1
-//       console.log(time)
-//       document.querySelector('.time').innerHTML = time
-//     } else {
-//       alert ('Time Over')
-//       clearInterval(interval)
-//       restart()
-//       gameStart()
-//       document.querySelector('.counter').innerHTML = '0'
-//       document.querySelector('.time').innerHTML = '30'
-//     }
-//   }
-// })
-// ---------------------------------------------------------
-// document.addEventListener('keydown', function () {
-//   var squareThree = gameArray[2] + 8
-//   buttons(event)
-//   if (time > 0) {
-//     if (numpad == squareThree) {
-//       tiles()
-//       counter++
-//       document.querySelector('.counter').innerHTML = counter
-//     } else {
-//       alert('Game Over')
-//       // clearInterval(interval)
-//       restart()
-//       gameStart()
-//       document.querySelector('.counter').innerHTML = '0'
-//       document.querySelector('.time').innerHTML = '30'
-//       tiles()
-//       }
-//   }
-// })
-// ---------------------------------------------------------
-function scoring () {
+function scoring (event) {
   var squareThree = gameArray[2] + 8
   if (event.keyCode === 90 || event.keyCode === 88 || event.keyCode === 78 || event.keyCode === 77) {
     if (numpad == squareThree) {
       counter++
       document.querySelector('.counter').innerHTML = counter
+    } else {
+      alert('Game Over')
+      restart()
+      clearInterval(test)
+      tile()
     }
   }
 }
 // ---------------------------------------------------------
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-16233284-1', 'auto');
+ga('send', 'pageview');
+// ---------------------------------------------------------
 
 document.addEventListener('keydown', function (event) {
-  var squareThree = gameArray[2] + 8
+  // var squareThree = gameArray[2] + 8
   buttons(event)
   intervalTimer()
-  tiles()
   scoring(event)
+  tiles()
   console.log(numpad)
-  console.log(squareThree)
+  // console.log(squareThree)
   console.log(event.keyCode)
 })
