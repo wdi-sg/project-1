816278e291e37e6b0f8e346cee178b9256fe8194
Peh Hui Shi <pehhuishi06@hotmail.com>
2017-10-01 16:26:35 +0800

    Able to remove mole from random position

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,39 @@
 $(function(){
 
   var score = 0;
-  var moleTimeout = 5;
+  var moleTimer = 5;
+  var gameTimeout = 180;
 
-  var $outter = $(".outter")
+  var $outer = $(".outer")
   var $holes =$('.holes')
+  var $button =$('button')
 
   alert('hello')
-  randomMoleAppear()
+
+  $button.on('click',randomMoleAppear)
+
 
 })
 
+//mole disappearance timeout is set to 5s, mole will disappear
 function randomMoleAppear(){
 
-  var random = Math.floor(Math.random() * 10);
+  var random = Math.floor(Math.random() * 5);
   console.log(random)
-  $('.outer').eq(random).addClass("image")
-  $('.holes').eq(random).removeClass()
+
+  $('.mole').eq(random).addClass("image")
+
+  function removeMole(){
+  $('.mole').eq(random).removeClass("image")
+  }
+  setTimeout(removeMole,4000)
+  console.log("test");
+
+}
+
+
+//within 5s user click it will score 10 points
+function whackMole(){
+
 
 }
