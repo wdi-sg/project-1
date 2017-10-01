c19f6e16ee6794db95d4ade282bf51bbce80dfae
Gabriel <gabrielulu@gmail.com>
2017-10-01 21:41:19 +0800

    Added winning logic and conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,30 +2,29 @@ $(function() {
 
   $body.on('keydown', ballMove)
 
-  // var position1 = $ball1.position()
-  // $body.on('click', () => {console.log($ball1.css('left'))}) //checking for position of ball1
-  // $body.on('click', () => {console.log(typeof($('.half').width()))})
-  // var position2 = $ball2.position()
   function ballMove(e) {
+    var unit = 10
     if (e.key === 'ArrowRight') {
-      if (borderCheck1Right()) ball1Hori(30) //if ball1 doesn't exceed right border
-      if (borderCheck2Left()) ball2Hori(30) //opposite side for ball2
+      if (ball1Goal() === false) if (borderCheck1Right()) ball1Hori(unit) //if ball1 doesn't exceed right border
+      if (ball2Goal() === false) if (borderCheck2Left()) ball2Hori(unit) //opposite side for ball2
+      // ball1Goal()
     }
     if (e.key === 'ArrowLeft') {
-      if (borderCheck1Left()) ball1Hori(-30)
-      if (borderCheck2Right()) ball2Hori(-30)
+      if (ball1Goal() === false) if (borderCheck1Left()) ball1Hori(-unit)
+      if (ball2Goal() === false) if (borderCheck2Right()) ball2Hori(-unit)
     }
   }
 
-  setInterval(ballSnap,500) //checking if border exceeds
   createGoal("20px","30px",$('#game1'))
   createGoal("20px","40px",$('#game2'))
-
+  setInterval(ballSnap,500) //checking if border exceeds
+  setInterval(levelClear,1000)
 })
 
 var $ball1 = $('#ball1')
 var $ball2 = $('#ball2')
 var $body = $('body')
+var goalLevel = 1 // to assign goal Id's for each play field
 
 //this controls horizontal ball movement
 function ball1Hori(a) {
@@ -42,28 +41,24 @@ function borderCheck1Right() {
   }
   else return false
 }
-
 function borderCheck2Right() {
   if (Number($ball2.css('left').replace("px","")) < $('.half').width() - $ball2.width()){
     return true
   }
   else return false
 }
-
 function borderCheck1Left() {
   if (Number($ball1.css('left').replace("px","")) >= 0){
     return true
   }
   else return false
 }
-
 function borderCheck2Left() {
   if (Number($ball2.css('left').replace("px","")) >= 0){
     return true
   }
   else return false
 }
-
 // snap ball back to edge of frame if it is out of play field
 function ballSnap() {
   if (borderCheck1Right() !== true) {
@@ -82,7 +77,7 @@ function ballSnap() {
 
 //goal creation constructor to customize goals in different levels
 class goal {
-  constructor (color = "black", width = "23px", height = "23px", top , left) {
+  constructor (color = "black", width = "23px", height = "23px", top, left, id) {
     this.color = color
     this.width = width
     this.height = height
@@ -90,20 +85,57 @@ class goal {
     this.left = left
   }
 }
-function createGoal(topPixels,leftPixels, half) {
+  function createGoal(topPixels,leftPixels, half) {
 
-  var goalCreate = new goal()
-  console.log(goalCreate)
+    var goalCreate = new goal()
+    console.log(goalCreate)
 
-  var $goalCreate = $('<div class = goal id = goal1>')
-  $goalCreate.css({
-    backgroundColor: goalCreate.color,
-    width: goalCreate.width,
-    height: goalCreate.height,
-    top: topPixels,
-    left: leftPixels,
-    position: 'relative',
-    display: 'inline-block'
-  })
-  half.append($goalCreate)
+    var $goalCreate = $('<div class = goal>')
+    $goalCreate.css({
+      backgroundColor: goalCreate.color,
+      width: goalCreate.width,
+      height: goalCreate.height,
+      top: topPixels,
+      left: leftPixels,
+      position: 'relative',
+      display: 'inline-block',
+      opacity: 0.5,
+      // float: 'left'
+    })
+    $goalCreate.attr('id',goalLevel.toString())
+    goalLevel ++
+    half.append($goalCreate)
+  }
+
+// to check if respective balls have reached their target
+function ball1Goal() {
+  console.log('checking ball1Goal')
+  if (($ball1.position().left < $('.goal:first').position().left + $('.goal:first').width()) &&
+      ($ball1.position().top < $('.goal:first').position().top + $('.goal:first').height()) &&
+      ($ball1.position().left + $ball1.width() > $('.goal:first').position().left) &&
+      ($ball1.position().top + $ball1.height() > $('.goal:first').position().top)){
+    $('.goal:first').css("backgroundColor","white")
+    return true
+  }
+  else return false
 }
+
+function ball2Goal() {
+  if (($ball2.position().left < $('.goal:last').position().left + $('.goal:last').width()) &&
+      ($ball2.position().top < $('.goal:last').position().top + $('.goal:last').height()) &&
+      ($ball2.position().left + $ball2.width() > $('.goal:last').position().left) &&
+      ($ball2.position().top + $ball2.height() > $('.goal:last').position().top)){
+    $('.goal:last').css("backgroundColor","white")
+    return true
+  }
+  else return false
+}
+
+function levelClear() {
+  if (ball1Goal() === true && ball2Goal() === true){
+    console.log('win')
+  }
+}
+// $body.on('click',($('.goal').eq(0).css("left",0)))
+// $body.on('click',() => {($ball1.css("left","60px"))})
+// $body.on('click',() => {($('.goal:first').css("left","0"))})
