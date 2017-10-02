403363299e05cb283e6418f7ba473f1be2a89ebf
yuki sy <yuki@Yukis-MacBook-Pro-2.local>
2017-10-02 23:38:34 +0800

    Updated Read Me and Javascript Functions

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 #  My Space Invader
 ![Space Invader Logo](https://nebula.wsimg.com/obj/QzRGNkYxNkJERTM0M0M0MDdCMzM6YWYxNzQ2ZGRmMmVkZWFiMmY2ZDVlZjQwMzgwNTY4ZTU6Ojo6OjA=)
 
+### Instructions How To Play
+
+  My Space Invader Game is designed for players who likes to play a never ending game where you rack up points until the player dies. The key to the game is to survive as long as possible while getting points by shooting enemies the the longer you survive more enemies will spawn. It's game over if one of the enemy sprite touches the player sprite. You win by beating your previous score.
 
 ![Space Invader Flowchart](/assets/img/spaceinvaderflowchart.jpg)
 
@@ -9,38 +12,53 @@
 
 1. background
 
-  ![My Background](/assets/img/starsbackground.jpg "SpaceInvaderLogo")
+  ![My Background](/assets/img/bg2.png)
+
+2. my sprites
+
+  ![My Player](/assets/img/ship.png)
+
+  ![My Laser](/assets/img/bullet.png)
+
+  ![Enemy](/assets/img/enemy1.png)
 
 
 ## Reference
 
-1.[Space Invader Tutorial](www.google.com)
+1.[Space Invader Tutorial](https://sites.google.com/site/wecanprogramcom/html-and-javascript/web-development-level2/lesson-1-setting-up-the-first-sprite)
 
 ## Code Snippet
 
-```
-var code =
+1. Create my gameLoop function to run my game smoothly
 
+```
+var lastLoopRun = 0
+
+function gameLoop() {
+  if (new Date().getTime() - lastLoopRun > 40) {
+      lastLoopRun = new Date().getTime()
+    }
+gameLoop()
 ```
 * I used a lot of `console.log`
 
 
-My Space Invader Game
+#### My Space Invader Game Pseudocode
 
-// Start
+1. Start
 
-// Load Game Map, Enemies, Player and Missile Sprite
+2. Load Game Map, Enemies, Player and Missile Sprite
 
-// Enemy Sprite spawns and flows from top to bottom of the page
-// I will make use of the functions I learned from the Factory of Balls Exercise to create a number of enemies that will be spawning from the game
+3. Enemy Sprite spawns and flows from top to bottom of the page
 
-// Game/Win or Lose Logic
+
+** Game/Win or Lose Logic
 1.	Player will be able to move around using “keydown” that will target the up, down, left, right, keys and spacebar of the keyboard. (spacebar will be for player to shoot missiles to the enemies)
 2.	Does the missile hit/touch an enemy sprite if (Yes) destroy the enemy sprite that collides with the missile/projectile else finish missile sprite until it reaches the top of the page or the top of axis-y
 3.	For each destroyed enemy sprite game will check if all enemy sprite has been destroyed if it reaches the maximum spawn number of enemy sprite and player is not dead then player wins if not continue spawning until it reaches maximum or until player is dead.
--	Player is dead/lose when enemy sprite collides with player sprite this will result in a game over and player will have to restart game.
+4.	Player is dead/lose when enemy sprite collides with player sprite this will result in a game over and player will have to restart game.
 
-// Game Over restart game if player wins or if player loses
+
 
 *Additional Ideas
 1. To create additional levels with a variety of difficulty such as faster enemy sprite or enemy with different sizes/radius or higher speed of spawns
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,22 +10,15 @@ player.h = 38
 var laser = new Object()
 laser.element = 'laser'
 laser.x = 0
-laser.y = -200
+laser.y = 0
 laser.w = 12
 laser.h = 15
 
-// var enemy = new Object()
-// enemy.element = 'enemy'
-// enemy.x = 0
-// enemy.y = -200
-// enemy.w = 50
-// enemy.h = 50
+var controller = new Object()
 
 //create an enemy Array
 var enemies = new Array()
 
-
-var controller = new Object()
 
 // keycode of controller
 var leftKey = 37
@@ -55,32 +48,17 @@ function pressKey(keyCode, isPressed) {
   }
 }
 
-// to keep the objects inside of the game map
-function ensureBounds(sprite) {
-  if (sprite.x < 5) {
-    sprite.x = 5
-  }
-  if (sprite.y < 5) {
-    sprite.y = 5
-  }
-  if (sprite.x + sprite.w > 495) {
-    sprite.x = 495 - sprite.w
-  }
-  if (sprite.y + sprite.h > 495) {
-    sprite.y = 495 - sprite.h
-  }
+document.onkeydown = function(event) {
+  pressKey(event.keyCode, true)
 }
 
-
-// set the position of our objects
-function setPosition(sprite) {
-  var e = document.getElementById(sprite.element)
-  e.style.left = sprite.x + 'px'
-  e.style.top = sprite.y + 'px'
+document.onkeyup =  function(event) {
+  pressKey(event.keyCode, false)
 }
 
 // input controls to player
 function handleControls() {
+
   if (controller.left) {
     player.x -= 5
   }
@@ -96,58 +74,19 @@ function handleControls() {
   if (controller.space && laser.y <= 0) {
     laser.x = player.x + 15
     laser.y = player.y - laser.h
+    $('#laser').css('visibility', 'visible')
+
   }
 
   ensureBounds(player)
-  // ensureBounds(laser)
-
 }
 
 
-
-// now to check for collision when element intersect with each other
-function intersect(a,b) {
-  return a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.y + a.h > b.y
-}
-
-// check for collision function
-  // laser collide with enemies
-  // hero collide with enemies
-  // and if uncontrollable object collide with edge of game map
-function checkCollision() {
-    for (var i = 0; i < enemies.length; i++) {
-      if (intersect(laser, enemies[i])) {
-        var element = document.getElementById(enemies[i].element);
-        element.style.visibility = 'hidden';
-        element.parentNode.removeChild(element);
-        enemies.splice(i, 1);
-        i--;
-        laser.y = -laser.h
-        score += 100
-      } else if (enemies[i].y + enemies[i].h >= 495) {
-        var element = document.getElementById(enemies[i].element);
-        element.style.visibility = 'hidden';
-        element.parentNode.removeChild(element);
-        enemies.splice(i, 1);
-        i--;
-        score += 50
-      } else if (intersect(player, enemies[i])) {
-        gameOver()
-      }
-    }
-}
-
-function gameOver() {
-  var element = document.getElementById(player.element);
-  element.style.visibility = 'hidden';
-  element = document.getElementById('gameover');
-  element.style.visibility = 'visible';
-
-  setInterval(reload, 5000)
-}
-
-function reload() {
-  location.reload()
+// set the position of our objects
+function setPosition(sprite) {
+  var e = document.getElementById(sprite.element)
+  e.style.left = sprite.x + 'px'
+  e.style.top = sprite.y + 'px'
 }
 
 var score = 0
@@ -168,51 +107,103 @@ function showSprites() {
 function updatePosition() {
   // update position of enemy
   for (var i = 0; i < enemies.length; i++) {
-    enemies[i].y += getRandom(5)
-    enemies[i].x += getRandom(5) - 2
+    enemies[i].y += getRandom(7)
+    enemies[i].x += getRandom(7) - 3
     ensureBounds(enemies[i], true)
   }
 
   // update position for the laser
-  laser.y -= 10
+  laser.y -= 15
 }
 
- var iterations = 0
+// to keep the objects inside of the game map
+function ensureBounds(sprite) {
+  if (sprite.x < 5) {
+    sprite.x = 5
+  }
+  if (sprite.y < 5) {
+    sprite.y = 5
+  }
+  if (sprite.x + sprite.w > 495) {
+    sprite.x = 495 - sprite.w
+  }
+  if (sprite.y + sprite.h > 495) {
+    sprite.y = 495 - sprite.h
+  }
+}
+
+var iterations = 0
 // add enemy to the game map
 function addEnemy() {
-    // how to add more enemy when time passes by
-    var interval = 50
-    if (iterations > 1500) {
-      interval = 5
-    } else if (iterations > 1000) {
-      interval = 20
-    } else if (iterations > 500) {
-      interval = 40
-    }
+   // how to add more enemy when time passes by
+   var interval = 50
+   if (iterations > 1500) {
+     interval = 5
+   } else if (iterations > 1000) {
+     interval = 20
+   } else if (iterations > 500) {
+     interval = 40
+   }
 
-    if (getRandom(interval) == 0) {
-      var elementName = 'enemy' + getRandom(10000000)
-      var enemy = new Object()
-      enemy.element = elementName
-      enemy.x = getRandom(450)
-      enemy.y = -50
-      enemy.w = 40
-      enemy.h = 40
+   if (getRandom(interval) == 0) {
+     var elementName = 'enemy' + getRandom(10000000)
+     var enemy = new Object()
+     enemy.element = elementName
+     enemy.x = getRandom(450)
+     enemy.y = -80
+     enemy.w = 40
+     enemy.h = 40
 
-      var createEnemy = document.createElement('div')
-      createEnemy.id = enemy.element
-      createEnemy.className = 'enemy'
-      $("#background").append(createEnemy)
+     var createEnemy = document.createElement('div')
+     createEnemy.id = enemy.element
+     createEnemy.className = 'enemy'
+     $("#background").append(createEnemy)
 
-      enemies[enemies.length] = enemy
-    }
-  }
-
-
+     enemies[enemies.length] = enemy
+   }
+ }
 
 // get random number for enemies
 function getRandom(max) {
-  return parseInt(Math.random() * max)
+ return parseInt(Math.random() * max)
+}
+
+
+// now to check for collision when element intersect with each other
+function intersect(a,b) {
+  return a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.y + a.h > b.y
+}
+
+// check for collision function
+  // laser collide with enemies
+  // hero collide with enemies
+  // and if uncontrollable object collide with edge of game map
+function checkCollision() {
+    for (var i = 0; i < enemies.length; i++) {
+      if (intersect(laser, enemies[i])) {
+        var element = document.getElementById(enemies[i].element);
+        element.style.visibility = 'hidden';
+        element.parentNode.removeChild(element);
+        enemies.splice(i, 1);
+        i--;
+        laser.y = -laser.h
+        $('#laser').css('visibility', 'hidden')
+        score += 100
+      } else if (intersect(player, enemies[i])) {
+        gameOver()
+      } else if (enemies[i].y + enemies[i].h >= 495) {
+        var element = document.getElementById(enemies[i].element);
+        element.style.visibility = 'hidden';
+        element.parentNode.removeChild(element);
+        enemies.splice(i, 1);
+        i--;
+      }
+    }
+    if (laser.y + laser.h <= 0) {
+    $('#laser').css('visibility', 'hidden')
+    // console.log($('#laser').position())
+    // console.log($('#background').position())
+    }
 }
 
 // this will run the events in our game to keep the gameplay smooth
@@ -221,7 +212,9 @@ var lastLoopRun = 0
 function gameLoop() {
   if (new Date().getTime() - lastLoopRun > 40) {
     updatePosition()
+
     handleControls()
+
     checkCollision()
 
     addEnemy()
@@ -231,15 +224,22 @@ function gameLoop() {
     lastLoopRun = new Date().getTime()
     iterations++
   }
-  setTimeout('gameLoop()', 2)
-}
-
-document.onkeydown = function(event) {
-  pressKey(event.keyCode, true)
-}
-
-document.onkeyup =  function(event) {
-  pressKey(event.keyCode, false)
+   setTimeout('gameLoop()', 2)
 }
 
 gameLoop()
+
+function gameOver() {
+  var element = document.getElementById(player.element);
+  element.style.visibility = 'hidden';
+  element = document.getElementById('gameover');
+  element.style.visibility = 'visible';
+
+
+  setInterval(reload, 10000)
+}
+
+
+function reload() {
+  location.reload()
+}
