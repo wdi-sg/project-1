779a734349635db772f8584c51faf66240f07a3c
Ray Tham <RayWTham@gmail.com>
2017-10-02 10:53:15 +0800

    change comments and var for gameboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,35 +3,52 @@ $(function () {
   var grid=[] //this is the game board
   var colorList = ['cubeA','cubeB','cubeC']
   var cellColor =""
+  var matrix = 4
 
   // create a 2D array
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Indexed_collections#Multi-dimensional_arrays
-  function generateLevel (matrix) {
+  function generateLevel () {
     //creates a new array of height $ width specified by 'matrix'
+    //create rows then columns, grid can be accessed with grid[row][column]
     grid = new Array(matrix)
-
-    //create rows X then columns Y, grid can be accessed with grid[x][y]
-    for (x = 0; x < matrix; x++) {
-      grid[x] = new Array(matrix)
-      for (y = 0; y < matrix; y++) {
-        grid[x][y] = '[' + x + ', ' + y + ']'
+    for (i = 0; i < matrix; i++) {
+      grid[i] = new Array(matrix)
+      for (j = 0; j < matrix; j++) {
+        // grid[x][y] = '[' + x + ', ' + y + ']'
+        //populate cells
+        grid[i][j] = generateColor()
       }
     }
 
-    //populate cells randomly
 
+
+    console.log(grid)
+    grid[1][1] = "test"
+    grid[1][2] = "test"
+    grid[1][3] = "test"
 
 
     console.log(grid)
   }
   function generateColor() {
     cellColor = colorList[Math.floor(Math.random() * colorList.length)]
+    return cellColor
   }
 
-  generateLevel(4)
-  generateColor()
+  function checkForMatch() {
+    for (var x = 0 ; x < matrix; x++) {
+      for (var y = 1; y < matrix; y++) {
+        if (grid[y] === grid[y-1] === grid[y+1]) console.log("success!")
+      }
 
-  console.log(grid)
-  console.log(cellColor)
+    }
+  }
+
+
+  generateColor()
+  generateLevel()
+  checkForMatch()
+
+
 
 })
