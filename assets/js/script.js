662ccf6415a4529f2542c8f51fcf13cf2cc8e08e
Nur Hidayah <dayah@Seths-MacBook.local>
2017-10-02 10:27:18 +0800

    added priceDiff fnc - result NaN

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,20 +10,43 @@ $submit2 = $('#submit-2')
 $p1Div = $('.player1Div')
 $p2Div = $('.player2Div')
 
+//prob, each time that someone clicks submit again, more numbers will appear.
+//definitely need to remove the submit and input text. do i hide it?
+
 $submit1.on('click', function () {
   var guess = ($('#player-1').val())
-  console.log(guess)
   var $printGuess = $("<h4 class = 'printGuess'>")
   $printGuess.text('$'+ guess)
   $p1Div.append($printGuess)
+  $("#player-1").remove();
+  $submit1.remove();
+  console.log(guess)
+  priceDiff() 
 })
 
 $submit2.on('click', function () {
   var guess = ($('#player-2').val())
-  console.log(guess)
   var $printGuess = $("<h4 class = 'printGuess'>")
   $printGuess.text('$'+ guess)
   $p2Div.append($printGuess)
+  $("#player-2").remove();
+  $submit2.remove();
+  console.log(guess)
 })
+
+function priceDiff() {
+  var itemPrice = $('img').data("price")
+  var p1Diff = $('#player-1').val() - itemPrice
+  console.log(p1Diff)
+}
+
+// priceDiff()
+//right now the priceDiff function is running first
+//need to run it under Reveal button
+//how to put reveal button
+
+
+
+
 
 })
