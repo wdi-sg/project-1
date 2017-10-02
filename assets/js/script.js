2ca4ed55b6301b226647c0490f8efec87791d489
koozy <koozhengyu@gmail.com>
2017-10-02 21:07:07 +0800

    create scoreboard, pacman movement, consume dots
    to update: score count logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // global variables
-
 var tileSet = [
   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
   1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -8,13 +7,13 @@ var tileSet = [
   1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1,
   1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1,
   1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 1,
-  1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1,
-  2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2,
+  1, 1, 1, 1, 1, 1, 2, 1, 1, 8, 1, 1, 2, 1, 1, 1, 1, 1, 1,
+  2, 2, 2, 2, 2, 2, 2, 1, 7, 6, 5, 1, 2, 2, 2, 2, 2, 2, 2,
   1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1,
   1, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 1,
   1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1,
   1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1,
-  1, 0, 0, 0, 0, 0, 1, 1, 0, 2, 0, 1, 1, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 1, 1, 0, 9, 0, 1, 1, 0, 0, 0, 0, 0, 1,
   1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1,
   1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1,
   1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -22,24 +21,87 @@ var tileSet = [
 ]
 
 var $body = $('body')
-var $pacMan = $('#pac-man')
 var $gameBoard = $('.game-board')
+
+// creating pac-man and ghosts
+var $pacMan = $('<div class="character" id="pac-man">')
+var $ghostOne = $('<div class="character" id="ghost-one">')
+var $ghostTwo = $('<div class="character" id="ghost-two">')
+var $ghostThree = $('<div class="character" id="ghost-three">')
+var $ghostFour = $('<div class="character" id="ghost-four">')
+var score = 0
+
+var $pacManTile = $pacMan.parent()
+var $pacManLeft = $pacManTile.prev()
+var $pacManRight = $pacManTile.next()
 
 $(function () {
   // creating map
   loadAssets(tileSet)
 
   // moving pacman
-  $body.on('keyup', (event) => { move(event) })
+  $body.on('keydown', (event) => { move(event) })
 })
 
 function move (event) {
-  var pacManPos = $pacMan.position()
+  var $pacMan = $('#pac-man')
+  var $pacManTile = $pacMan.parent()
+  var $pacManLeft = $pacManTile.prev()
+  var $pacManRight = $pacManTile.next()
+  var $pacManDown = $pacManTile.nextAll().eq(18)
+  var $pacManUp = $pacManTile.prevAll().eq(18)
+  var $score = $('#score')
 
-  if(event.key === 'ArrowUp' && pacManPos.top !== 0) $pacMan.css('top', `${pacManPos.top - 30}px`)
-  else if(event.key === 'ArrowDown' && pacManPos.top !== 510) $pacMan.css('top', `${pacManPos.top + 30}px`)
-  else if(event.key === 'ArrowLeft' && pacManPos.left !== 0) $pacMan.css('left', `${pacManPos.left - 30}px`)
-  else if(event.key === 'ArrowRight' && pacManPos.left !== 570) $pacMan.css('left', `${pacManPos.left + 30}px`)
+  switch(event.key) {
+    case 'ArrowUp':
+      // check if tile has dots
+      if ($pacManUp.has('.dots')) {
+        score++
+        $score.text(`Score: ${score}`)
+        $pacManUp.empty()
+      }
+      // check if pac-man can move into tile
+      if ($pacManUp.data('attr') !== 1) {
+        $pacManUp.append($pacMan)
+      }
+      break
+    case 'ArrowRight':
+      // check if tile has dots
+      if ($pacManRight.has('.dots')) {
+        score++
+        $score.text(`Score: ${score}`)
+        $pacManRight.empty()
+      }
+      // check if pac-man can move into tile
+      if ($pacManRight.data('attr') !== 1) {
+        $pacManRight.append($pacMan)
+      }
+      break
+    case 'ArrowDown':
+      // check if tile has dots
+      if ($pacManDown.has('.dots')) {
+        score++
+        $score.text(`Score: ${score}`)
+        $pacManDown.empty()
+      }
+      // check if pac-man can move into tile
+      if ($pacManDown.data('attr') !== 1) {
+        $pacManDown.append($pacMan)
+      }
+      break
+    case 'ArrowLeft':
+      // check if tile has dots
+      if ($pacManLeft.has('.dots')) {
+        score++
+        $score.text(`Score: ${score}`)
+        $pacManLeft.empty()
+      }
+      // check if pac-man can move into tile
+      if ($pacManLeft.data('attr') !== 1) {
+        $pacManLeft.append($pacMan)
+      }
+      break
+  }
 }
 
 function loadAssets (tileSet) {
@@ -48,19 +110,46 @@ function loadAssets (tileSet) {
     var $tile = $('<div class="tile">')
     var $dots = $('<div class="dots">')
     // setting tile properties
-    if (tileSet[i] === 0) {
-      $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
-      $tile.data('attr', 0)
-      $tile.append($dots)
-    } else if (tileSet[i] === 1) {
-      $tile.css({ 'background-color': 'blue', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
-      $tile.data('attr', 1)
-    }  else if (tileSet[i] === 2) {
-      $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
-      $tile.data('attr', 2)
+    switch (tileSet[i]) {
+      case 0:
+        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.data('attr', 0)
+        $tile.append($dots)
+        break
+      case 1:
+        $tile.css({ 'background-color': 'blue', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.data('attr', 1)
+        break
+      case 2:
+        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.data('attr', 2)
+        break
+      case 9:
+        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.data('attr', 9)
+        $tile.append($pacMan)
+        break
+      case 8:
+        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.data('attr', 8)
+        $tile.append($ghostOne)
+        break
+      case 7:
+        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.data('attr', 7)
+        $tile.append($ghostTwo)
+        break
+      case 6:
+        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.data('attr', 6)
+        $tile.append($ghostThree)
+        break
+      case 5:
+        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.data('attr', 5)
+        $tile.append($ghostFour)
+        break
     }
-
-    // append tile to .game-board
     $gameBoard.append($tile)
   }
 }
