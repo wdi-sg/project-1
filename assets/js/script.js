c2011fa451782d20a15ce3b6f2f627348dabec64
koozy <koozhengyu@gmail.com>
2017-10-02 21:40:16 +0800

    update scoring logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// global variables
+// tileSet array (for generating map)
 var tileSet = [
   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
   1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -20,8 +20,12 @@ var tileSet = [
   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
 ]
 
+// jQuery objects
 var $body = $('body')
 var $gameBoard = $('.game-board')
+
+// score variable
+var score = 0
 
 // creating pac-man and ghosts
 var $pacMan = $('<div class="character" id="pac-man">')
@@ -29,11 +33,6 @@ var $ghostOne = $('<div class="character" id="ghost-one">')
 var $ghostTwo = $('<div class="character" id="ghost-two">')
 var $ghostThree = $('<div class="character" id="ghost-three">')
 var $ghostFour = $('<div class="character" id="ghost-four">')
-var score = 0
-
-var $pacManTile = $pacMan.parent()
-var $pacManLeft = $pacManTile.prev()
-var $pacManRight = $pacManTile.next()
 
 $(function () {
   // creating map
@@ -54,25 +53,25 @@ function move (event) {
   switch(event.key) {
     case 'ArrowUp':
       // check if tile has dots
-      if ($pacManUp.children().length > 0) eatAndChangeScore($pacManUp)
+      if ($pacManUp.has('.dots').length > 0) eatAndChangeScore($pacManUp)
       // check if pac-man can move into tile
       if ($pacManUp.data('attr') !== 1) $pacManUp.append($pacMan)
       break
     case 'ArrowRight':
       // check if tile has dots
-      if ($pacManRight.children().length > 0) eatAndChangeScore($pacManRight)
+      if ($pacManRight.has('.dots').length > 0) eatAndChangeScore($pacManRight)
       // check if pac-man can move into tile
       if ($pacManRight.data('attr') !== 1) $pacManRight.append($pacMan)
       break
     case 'ArrowDown':
       // check if tile has dots
-      if ($pacManDown.children().length > 0) eatAndChangeScore($pacManDown)
+      if ($pacManDown.has('.dots').length > 0) eatAndChangeScore($pacManDown)
       // check if pac-man can move into tile
       if ($pacManDown.data('attr') !== 1) $pacManDown.append($pacMan)
       break
     case 'ArrowLeft':
       // check if tile has dots
-      if ($pacManLeft.children().length > 0) eatAndChangeScore($pacManLeft)
+      if ($pacManLeft.has('.dots').length > 0) eatAndChangeScore($pacManLeft)
       // check if pac-man can move into tile
       if ($pacManLeft.data('attr') !== 1) $pacManLeft.append($pacMan)
       break
@@ -91,43 +90,45 @@ function loadAssets (tileSet) {
     // create new <div>
     var $tile = $('<div class="tile">')
     var $dots = $('<div class="dots">')
+    var blackTile = { 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' }
+    var blueTile = { 'background-color': 'blue', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' }
     // setting tile properties
     switch (tileSet[i]) {
       case 0:
-        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.css(blackTile)
         $tile.data('attr', 0)
         $tile.append($dots)
         break
       case 1:
-        $tile.css({ 'background-color': 'blue', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.css(blueTile)
         $tile.data('attr', 1)
         break
       case 2:
-        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.css(blackTile)
         $tile.data('attr', 2)
         break
       case 9:
-        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.css(blackTile)
         $tile.data('attr', 9)
         $tile.append($pacMan)
         break
       case 8:
-        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.css(blackTile)
         $tile.data('attr', 8)
         $tile.append($ghostOne)
         break
       case 7:
-        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.css(blackTile)
         $tile.data('attr', 7)
         $tile.append($ghostTwo)
         break
       case 6:
-        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.css(blackTile)
         $tile.data('attr', 6)
         $tile.append($ghostThree)
         break
       case 5:
-        $tile.css({ 'background-color': 'black', 'height': '28px', 'width': '28px', 'border': '1px solid yellow' })
+        $tile.css(blackTile)
         $tile.data('attr', 5)
         $tile.append($ghostFour)
         break
