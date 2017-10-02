0f2aa6543610b96d40c449e0404aff0a70753ff9
subutai1175 <subutai1175@gmail.com>
2017-10-02 14:29:57 +0800

    added functions to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,30 @@
 $(function(){
-
-function ()
-
-
-
-
+var recArr = [];
+var $box = $('.box')
+var $locBtn = $('#loc')
+var n = 0;
 
 
+$box.on('click', locGen)
+$locBtn.on('click', compareResult)
 
 
+function locGen() {
+  var index = randomNum();
+  // $(`${index}`).css("backgroundColor","blue")
+  recArr.push(index);
+  $(this).css('backgroundColor', '#839073')
+  n++;
+}
 
+function randomNum () {
+  var index = Math.floor(Math.random() * 8);
+  return index;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+function compareResult () {
+  
+}
 
 
 
