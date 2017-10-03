e551fadf323b82e2ccb88e60d65b2280455c00ee
subutai1175 <subutai1175@gmail.com>
2017-10-03 14:14:07 +0800

    start stop logic, button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,39 @@
 $(function(){
-var recArr = [];
 var $box = $('.box')
 var $locBtn = $('#loc')
+var $startBtn = $('#startBtn')
+var $score = $('#scoreBoard');
+var recArr = [];
 var count = 0;
-var score = 0;
 var index = 0;
+var on = 0;
+var refreshIntervalId;
+
 
 $box.on('click', locGen)
 $locBtn.on('click', compareResult)
+$startBtn.on('click', () => {
+  if (on == 0) {
+  refreshIntervalId = setInterval(locGen, 3000);
+  on = 1;
+} else if (on == 1) {
+  clearInterval(refreshIntervalId);
+  on = 0;
+}
 
-// setInterval(locGen, 3000);
+});
 
 function locGen() {
   count++;
-  if (count == 20) {
-    gameOver()//call gameover
+  if (count == 5) {
+    $('.box').css("backgroundColor","")
+    return gameOver()//call gameover
   }
   // console.log(count);
-  console.log(index)
+  // console.log(index)
   $(`[data-value = ${index}]`).css("backgroundColor","")
   index = randomNum();
-  console.log(index)
+  // console.log(index)
   // $(this).data('value')
 
   setTimeout(function() {
@@ -33,28 +46,31 @@ function locGen() {
 
 
 function compareResult () {
-  console.log('check recArr:', recArr)
-  console.log('check recArr[count]:', recArr[count - 1])
-  console.log('check recArr[count - 1]:', recArr[count - 2])
-  if ( recArr[count - 1] === recArr[count - 2]) {
-    console.log('entered!')
-    score+= 10;
+  // console.log('check recArr:', recArr)
+  // console.log('check recArr[count]:', recArr[count - 1])
+  // console.log('check recArr[count - 1]:', recArr[count - 2])
+  if (recArr[count - 1] === recArr[count - 2]) {
+    // console.log('entered!')
+    $score+= 10;
   }
 }
 
 function randomNum () {
   var index = Math.floor(Math.random() * 8);
-  console.log(index)
+  // console.log(index)
   return index;
 }
 
 function gameOver () {
   // Show Score.
   count = 0;
-  score = 0;
+  $score = 0;
   index = 0;
   recArr = [];
-  console.log('Count:', count, "Score:", score, "Index:", index, "recArr:", recArr)
+  console.log('Count:', count, "Score:", $score, "Index:", index, "recArr:", recArr)
+  // console.log(clearInterval, refreshIntervalId);
+  clearInterval(refreshIntervalId);
+  // console.log(clearInterval, refreshIntervalId);
   // reset gameboard
   // off setInterval
 }
