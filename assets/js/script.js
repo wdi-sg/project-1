1216cd56945b79d24b2cba23c9f6da0d421d858d
Shui <shuiloong@gmail.com>
2017-10-04 08:50:15 +0800

    uploaded images

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,30 @@
 
 // global
 class Flower {
-  constructor (color = this.colorFn()) {
-    this.color = color
-    this.element = $('<div class="flower">')
+  constructor () {
+    this.index = this.colorFn()
+    this.color = this.index.color
+    this.points = this.index.points
+    this.element = $(`<div class="flower" data-score=${this.points}>`)
 
     setInterval(() => {
       this.fall()
-    }, 100)
+    }, 180)
   }
 
   colorFn () {
-    var colorSet = ['#DB5461', '#6D1135', '#5C7ECD', '#FFEF80', '#63C5B7']
+    var colorSet = [{color:'black', points: -5},
+                    {color:'black', points: -4},
+                    {color:'#5C7ECD', points: 3},
+                    {color:'#FFEF80', points: 2},
+                    {color:'#63C5B7', points: 1}]
+
+    // for (var i = 0; i < colorSet.length; i++)
     var colorIndex = Math.floor(Math.random() * colorSet.length)
     return colorSet[colorIndex]
+
   }
+
   fall () {
     // console.log("flower is falling")
     // get the element of the flower &
@@ -25,10 +35,29 @@ class Flower {
     var position = this.element.position()
 
     this.element.css('top', position.top + 30 + 'px')
-    if (position.top > '520') {
+    if (position.top > '600') {
       this.element.remove()
     }
   }
+
+  // detect () {
+  //   var $playerOne = $('.playerOne')
+  //   var $playerPos = $playerOne.position()
+  //   var $flower = $('.flower')
+  //   var $flowerPos = $flower.position()
+  //   var $score = $('.score')
+  //
+  //   for (key in flowerList) {
+  //     if ($flowerPos.left < $playerPos.left + $playerOne.width() &&
+  //     $flowerPos.left + $playerOne.width() > $playerPos.left &&
+  //     $flowerPos.top < $playerPos.top + $playerOne.height() &&
+  //     $flower.height() + $playerPos.top > $playerPos.top) {
+  //       console.log('hit')
+  //
+  //     }
+  //   }
+  // }
+
 }
 
 class Player {
@@ -38,17 +67,24 @@ class Player {
   }
 }
 
+
 $(function () {
   var $playerOne = $('.playerOne')
   var $container = $('.container')
   var $flower = $('.flower')
   var $playerPos = $playerOne.position()
   var $flowerPos = $flower.position()
+  var score = 0
+  var timer = 30
 
-  setInterval(createFlower, 140)
-  createFlower()
+  setInterval(createFlower, 300)
+
+  var firstFlower = new Flower()
   createPlayerOne()
-  setInterval(detect, 140)
+  // setInterval($flower.detect, 100)
+  setInterval(detect, 300)
+  var flowerList =[]
+  var flowerIndex = 0
 
   function createFlower () {
     var randomFlower = new Flower()
@@ -57,9 +93,18 @@ $(function () {
       backgroundColor: randomFlower.color,
       left: (Math.floor(Math.random() * 1350) + 'px')
     })
-
+    flowerList[flowerIndex] = randomFlower
+    // console.log(randomFlower)
+    flowerIndex++
     $container.append($newFlower)
   }
+
+  $timer = $('.timer')
+  setInterval( () => {
+    timer = timer - 1
+    $timer.text("Time : " + timer)
+  } ,1000)
+
 
   function createPlayerOne () {
     var player = new Player()
@@ -81,20 +126,31 @@ $(function () {
     }
   })
 
-  function score () {
-    var scoreBoard = $('.score')
-  }
-
   function detect () {
     var $playerOne = $('.playerOne')
     var $playerPos = $playerOne.position()
     var $flower = $('.flower')
     var $flowerPos = $flower.position()
+    var $score = $('.score')
 
-    if ($playerPos.left < $flowerPos.left + $flower.width() &&
-        $playerPos.left + $flower.width() > $flowerPos.left &&
-        $playerPos.top < $flowerPos.top + $flower.height() &&
-        $playerOne.height() + $flowerPos.top > $flowerPos.top) console.log('hit')
-  // collision detected!
+
+    for (key in flowerList) {
+      // console.log(flowerList[key]['score']);
+      if ($playerPos.left < $flowerPos.left + $flower.width() &&
+      $playerPos.left + $flower.width() > $flowerPos.left &&
+      $playerPos.top < $flowerPos.top + $flower.height() &&
+      $playerOne.height() + $flowerPos.top > $flowerPos.top) {
+        console.log('hit')
+        // console.log($(flowerList))
+        score += flowerList[key].points
+        flowerList.splice(key, 1)
+        $score.text(`Score: ${score}`)
+      }
+    }
   }
+
+  function gameStart() {
+
+  }
+
 })
