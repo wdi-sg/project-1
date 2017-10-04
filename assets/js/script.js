0101f0a81989f0f132ca7c4854f93898fa228b10
G174 <31766585+G174@users.noreply.github.com>
2017-10-04 14:39:49 +0800

    project 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,66 @@ $(function(){
   "url(/assets/img/italy.png)","url(/assets/img/mexico.png)","url(/assets/img/india.png)"]
 
   var score = 0
+  var randomIndex
+  var usedIndex = []
+  var begin
 
-  //shuffleImg for next play
-  shuffleImg()
-  //Hides 1 box every 1s
-  var start = setInterval(hideBox,1000)
+  //start on click
+  $('.start').on('click', start)
+
+  function start() {
+    //clear usedIndex
+    usedIndex = []
+    //score reset
+    score = 0
+    //shuffleImg for next play
+    shuffleImg()
+    //Hides 1 box every 1s
+    begin = setInterval(hideBox, 1000)
+    // calling timer object method to create timer upon page load
+    timerObj.startTimer()
+    if (randomIndex === 0) {
+      $('<input type="text" class="input" placeholder="*******">').insertBefore(".submit");
+    } else if (randomIndex === 1) {
+      $('<input type="text" class="input" placeholder="*****">').insertBefore(".submit");
+    } else if (randomIndex === 2) {
+      $('<input type="text" class="input" placeholder="*****">').insertBefore(".submit");
+    } else if (randomIndex === 3) {
+      $('<input type="text" class="input" placeholder="******">').insertBefore(".submit");
+    } else if (randomIndex === 4) {
+      $('<input type="text" class="input" placeholder="*****">').insertBefore(".submit");
+    }
+  }
+
+  // timer object (creates timer)
+  var timerObj = {
+    // object keys
+    seconds: 0,
+    timerId: 0,
+    // embedded object
+    els: {
+      timer: $('#timer')
+    },
+    // object method, start timer on page load
+    startTimer() {
+      this.els.timer.text(`Time elapsed: ${this.seconds}`)
+      this.timerId =  setInterval(() => {
+        this.seconds++
+        this.els.timer.text(`Time elapsed: ${this.seconds}`)
+      },1000)
+    },
+    // object method, updates timer
+    updateTime (){
+      this.seconds++;
+      this.els.timer.textContent = 'Time elapsed: ' + this.seconds;
+    },
+    //object method, reset timer
+    resetTime() {
+      clearInterval(this.timerId)
+      this.seconds = 0
+    }
+  }
+
   //generate random number to hide box in grid
   function hideBox() {
     var random = Math.floor(Math.random()*25)
@@ -21,80 +76,27 @@ $(function(){
   }
 
 
-var randomIndex
-
  //shuffle image for next question
   function shuffleImg() {
-    var getRand = (function() {
-    var nums = [0,1,2,3,4,];
-    var current = [];
-    function rand(n) {
-        return (Math.random() * n)|0;
+    // console.log(usedIndex)
+    randomIndex = Math.floor(Math.random()*5)
+    if (usedIndex.length < 5){
+    while (usedIndex.includes(randomIndex)) {
+      randomIndex = Math.floor(Math.random()*5)
+      console.log('inside while loop', usedIndex)
     }
-    return function() {
-      if (!current.length) current = nums.slice();
-      return current.splice(rand(current.length), 1);
-    }
-}());
-    // randomIndex = Math.floor(Math.random()*)
-    answer = country[nums]
-    var flagImage = image[nums]
+    usedIndex.push(randomIndex)
+  }
+    answer = country[randomIndex]
+    var flagImage = image[randomIndex]
     var $container = $(".container")
     $container.css("background-image",flagImage)
   }
-  // timer object (creates timer)
-  var timerObj = {
-    // object keys
-    seconds: 0,
-    timerId: 0,
-
-    // embedded object
-    els: {
-      timer: $('#timer')
-    },
-
-    // object method, start timer on page load
-    startTimer() {
-      this.els.timer.text(`Time elapsed: ${this.seconds}`)
-
-      this.timerId =  setInterval(() => {
-            this.seconds++
-            this.els.timer.text(`Time elapsed: ${this.seconds}`)
-          },1000)
-        },
-    // object method, updates timer
-    updateTime (){
-      this.seconds++;
-      this.els.timer.textContent = 'Time elapsed: ' + this.seconds;
-    }
-  }
-  // calling timer object method to create timer upon page load
-  timerObj.startTimer()
 
 
-  // on click method to remove boxes
-  // $box.on('click', function() {
-  //   $(this).css('visibility', 'hidden')
-  // })
-
-//append textbox as new tag
-  // var $letters = $('.letters')
-
-if (randomIndex === 0) {
-
-  $('<input type="text" class="input" placeholder="*******">').insertBefore(".submit");
-} else if (randomIndex === 1) {
-  $('<input type="text" class="input" placeholder="*****">').insertBefore(".submit");
-} else if (randomIndex === 2) {
-  $('<input type="text" class="input" placeholder="*****">').insertBefore(".submit");
-} else if (randomIndex === 3) {
-  $('<input type="text" class="input" placeholder="******">').insertBefore(".submit");
-} else if (randomIndex === 4) {
-  $('<input type="text" class="input" placeholder="*****">').insertBefore(".submit");
-}
 
   //check answer for match on click
-  $('#submit').click(function(){
+  $('.submit').click(function() {
     var $input = $('.input')
     submitAnswer = $input.val()
     checkAnswer()
@@ -103,40 +105,48 @@ if (randomIndex === 0) {
   })
 
     //does submitAnswer match answer
-    function checkAnswer() {
+  function checkAnswer() {
     if(submitAnswer === answer && timerObj.seconds < 5) {
       score += 2
       $('span').text(score)
+      isGameOver()
       reset()
+
     } else if (submitAnswer === answer && timerObj.seconds <= 30) {
       score += 1
       $('span').text(score)
+      isGameOver()
       reset()
-    } else if(submitAnswer !== answer && timerObj.seconds >30) {
+
+    } else if(submitAnswer !== answer || timerObj.seconds >30) {
       score += 0
+      isGameOver()
       reset()
+
     }
   }
 
-var end = setInterval(isGameOver,)
+// var end = setInterval(isGameOver,)
   function isGameOver() {
-    if (score < 5) {
+    if (usedIndex.length === 5 && score < 5) {
+      alert('fail')
+      $('input').remove()
+      start()
+    } else if (score >= 5) {
+    alert('proceed to level 2')
     reset()
-  } else if (score >= 5){
-    $(.endPage).css('visibility','visible')
-    $(.scoreCounter).css('display','none')
-    $container.css('display', 'none')
-    $(.letters).css('display','none')
-    $(.timer).css('display','none')
   }
 }
 
   function reset() {
     //reset timer
-    clearInterval(start)
+    timerObj.resetTime()
+    timerObj.startTimer()
     //reset grid
+    clearInterval(begin)
     $box.css('visibility', 'visible')
     shuffleImg()
     setInterval(hideBox,1000)
   }
+
 })
