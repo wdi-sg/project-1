8ce311fa6bd541f30607ed7333ab9d4b3c6bd02f
josephpung <goldenpummel@live.com>
2017-10-05 20:23:46 +0800

    edited README and added comments to js file

--- README.md
+++ README.md
@@ -7,7 +7,13 @@ Game is based on **Maplestory**, a Mass Multiplayer Online Role Playing Game (MM
 - Space to jump
 - "C" to attack
 - "D" for skill 1
+- "X" for skill 2
+- "Z" for skill 3
+##### Instructions
 
+Control your character and avoid dying to the mobs spawned, your character has a mana pool as well as a hitpoint pool. If your mana pool is depleted, you will not be able to use any skills till it regenerates. Once your hitpoint pool reaches zero, you die and the game ends.
+
+Kill enough mobs and the boss awaits you!
 
 ### Log
 Currently working on making the initial base fighting area and the main player
@@ -15,11 +21,18 @@ Currently working on making the initial base fighting area and the main player
 * 2/10/17 - Changed CSS styles and edit to fix axis differences
 * 3/10/17 - Added health bar and player hit points
 * 4/10/17 - Added mana regen and mana bar, replaced manual spawn with class based spawning and added various functions to check for game status.
+* 5/10/17 - Added boss and 2 more skills to character, edited main skill and attack functions to accomodate to boss feature
 
+### Image Log
+**29/10/17 (Initial Draft Frame)**
+![](/assets/images/old.gif)
+**4/10/17**
 ![](/assets/images/v2.png)
+
 ### **BUG REPORT**
   * Jump is still choppy, unable to remove 1 sec delay from continuous button pressing.
   * Collision detection for far left side of the map working properly (0-200px)
+  * Mobs occasionally not dying, pressing skill button kills them and ends bug
 
 
 ### Flowchart
--- assets/js/testScript.js
+++ assets/js/testScript.js
@@ -1,10 +1,10 @@
-class Mob {
+class Mob { // class to spawn mobs
 
   constructor (id) {
     this.id = id
     this.hitPoints = 20
-    this.damage = 1
-    this.bosscheck = false
+    this.damage = 1 // mob damage per second
+    this.bosscheck = false // check to allow DOM to check whether its a boss or a mob spawn
     this.jTarget = $('body').find(`#${this.id}`)
     this.width = this.jTarget.css('width')
     this.height = this.jTarget.css('height')
@@ -16,7 +16,7 @@ class Mob {
 
   positionCheck () {
     var speed = Math.floor(Math.random() * 20)
-    var mobPosition = this.jTarget.position()
+    var mobPosition = this.jTarget.position() // get position of current mob
     if (mobPosition.left < 500) {
       this.jTarget.css({
         width: '180px',
@@ -26,14 +26,14 @@ class Mob {
         top: '505px',
         left: '1460px',
         transition: `ease-out ${speed}s`,
-        background: 'url(./assets/images/stoneGolemRight.png)',
-        animation: 'mobPlayRight 2s steps(4) infinite'
+        background: 'url(./assets/images/stoneGolemRight.png)', // assign right facing image when mob turns
+        animation: 'mobPlayRight 2s steps(4) infinite' // run animation
       })
     } else if (mobPosition.left > 1400) {
       this.jTarget.css({
         left: '1px',
         transition: `ease-out ${speed}s`,
-        background: 'url(./assets/images/stoneGolem.png)',
+        background: 'url(./assets/images/stoneGolem.png)', // assign left facing image when mob turns
         animation: 'mobPlay 2s steps(4) infinite'
       })
     }
@@ -60,12 +60,11 @@ class Mob {
 
 }
 
-
-class Boss {
+class Boss {  // Class to spawn boss
 
   constructor (id) {
     this.id = id
-    this.hitPoints = 100
+    this.hitPoints = 350
     this.damage = 3
     this.bosscheck = true
     this.jTarget = $('body').find(`#${this.id}`)
@@ -124,13 +123,12 @@ $(function () {
   var $player = $('.player')
   var $bullet = $('.projectile')
   var $body = $('body')
-  var $mobHP = $('#mobHP')
   var $playerHP = $('#healthPoints')
   var $hpBar = $('.hpBar')
   var $startButton = $('#restart')
   var $firstSkillDiv = $('.skillOne')
   var $mpBar = $('.mpBar')
-  var $bossSign = $(".levelOneGameEnd")
+  var $bossSign = $('.levelOneGameEnd')
   var mobArray = []
   var bossArray = []
   var fireDirection = ''
@@ -140,31 +138,28 @@ $(function () {
   var mobsOnScreen = 0
   var levelOneMobCount = 10
   var gameEnd = false
-  var bossDead =false
+  var bossDead = false
   var bossLevel = false
   var mana = 202
   $bullet.hide()
 
+  $startButton.on('click', restartGame)
+
   setInterval(function () {
-    if (gameEnd) { $('.landingScreen').show()}
-
+    if (gameEnd) { $('.landingScreen').show() } // if game has ended show the landing screen
   }, 1000)
-  setInterval(function(){
-    if (bossLevel){ $bossSign.css("visibility","visible")
-    setTimeout(function () {
-      $bossSign.css('visibility', 'hidden')
-    }, 800)
-    if(mobsOnScreen <1 && bossDead === false){generateBoss()}
-  }
-},1000)
 
-  $startButton.on('click', restartGame) 
+  setInterval(function () {                 // check whether boss is to be spawned, if yes set image to visible and spawn boss
+    if (bossLevel) {
+      $bossSign.css('visibility', 'visible')
+      setTimeout(function () {
+        $bossSign.css('visibility', 'hidden')
+      }, 2000)
+      if (mobsOnScreen < 1 && bossDead === false) { generateBoss() } // ensure no other bosses are on screen prior to spawning
+    }
+  }, 1000)
 
-  function levelOneResultPage () {
-
-  }
-
-  function restartGame () {
+  function restartGame () {    // function to reset all fields when game restarts
     $('.landingScreen').hide()
     playerHealth = 1000
     $hpBar.text(`${playerHealth}/1000`)
@@ -181,14 +176,14 @@ $(function () {
     for (key in mobArray) {
       mobArray[key].jTarget.remove()
     }
-    mobArray =[]
+    mobArray = []
     for (key in bossArray) {
       bossArray[key].jTarget.remove()
     }
-    bossArray=[]
+    bossArray = []
   }
 
-  function runSpawner () {
+  function runSpawner () {  // spawn mobs based on limit set per level
     var mobSpawn = setInterval(function () {
       if (mobsOnScreen < levelOneMobCount) {
         generateMob()
@@ -196,42 +191,42 @@ $(function () {
     }, 3000)
   }
 
-  function generateMob () {
+  function generateMob () {     //run mob spawn class
     var id = Math.floor(Math.random() * 1000)
     mobArray[id] = new Mob(id)
     mobArray[id].addChar()
     mobsOnScreen += 1
   }
-  function generateBoss () {
+  function generateBoss () {      // run boss spawn class
     var id = Math.floor(Math.random() * 1000)
     bossArray[id] = new Boss(id)
     bossArray[id].addBoss()
-    mobsOnScreen +=1
+    mobsOnScreen += 1
   }
 
-  var manaRegen = setInterval(function () {
+  var manaRegen = setInterval(function () {  //mana regeneration function
     if (mana < 200) {
-      mana += 1
+      mana += 4
       $mpBar.text(`${mana}/200`)
-      $mpBar.css('width', `${$mpBar.width() + 1}px`)
+      $mpBar.css('width', `${$mpBar.width() + 4}px`)
     }
   }, 1000)
 
-  setInterval(function () {
+  setInterval(function () {       // collision check for bullets and player contact against mobs
     for (key in mobArray) {
       bulletCollisionCheck($bullet, mobArray[key])
       playerMobCollisionCheck($player, mobArray[key])
     }
   }, 30)
 
-  setInterval(function () {
+  setInterval(function () {       // collision check for bullets and player contact against boss
     for (key in bossArray) {
       bulletCollisionCheck($bullet, bossArray[key])
       playerMobCollisionCheck($player, bossArray[key])
     }
   }, 30)
 
-  $body.on('keydown', function (e) {
+  $body.on('keydown', function (e) {  //player keyboard press detection
     var $player = $('.player')
     var $playPos = $player.position()
     keys[e.keyCode] = true
@@ -249,24 +244,24 @@ $(function () {
       $player.css('transform', 'scaleX(1)')
       fireDirection = 'left'
     }
-    if (keys[37] && $playPos.left > 151) {
+    if (keys[37] && $playPos.left > 151) {        // left
       $player.css('left', `${$playPos.left -= 150}px`)
       $player.css('transform', 'scaleX(1)')
       fireDirection = 'left'
-    } // left
+    }
     if (keys[32] && $playPos.top === 560) { $player.css('top', `${$playPos.top -= 300}px`) }   // up
     if (keys[39] && $playPos.left < 1480) {
       $player.css('left', `${$playPos.left += 150}px`)
       $player.css('transform', 'scaleX(-1)')
       fireDirection = 'right'
     } // right
-    if (keys[67]) { fireBullet() }
-    if (keys[68]) { skillOne() }
-    if (keys[90]) { skillTwo() }
-    if (keys[88]) { skillThree() }
+    if (keys[67]) { fireBullet() } //default attack
+    if (keys[68]) { skillOne() } // purple wave (D)
+    if (keys[90]) { skillTwo() } // giant panda (X)
+    if (keys[88]) { skillThree() }// giant hammer(Z)
     e.preventDefault() // prevent the default action (scroll / move caret)
   })
-  $body.on('keyup', function (e) {
+  $body.on('keyup', function (e) {    // stop player sprite animation when not moving or attacking and reset player Y-axis to counteract jump
     keys[e.which] = false
     var $player = $('.player')
     var $playPos = $player.position()
@@ -275,7 +270,7 @@ $(function () {
     if ($playPos.top !== 560) { $player.css('top', `560px`) }
   })
 
-  function bulletCollisionCheck (obj1, obj2) {
+  function bulletCollisionCheck (obj1, obj2) { //bullet collision check
     var red = obj1.position()
     var redX = red.left
     var blue = obj2.jTarget.position()
@@ -291,26 +286,26 @@ $(function () {
       $bullet.css('top', '620px')
       if (obj2.hitPoints === 0) {
         obj2.jTarget.remove()
-        if(obj2.bosscheck === false){
-        mobArray.splice(key, 1)
-        mobsOnScreen--
-        levelOneMobCount--
-      }else if(obj2.bosscheck){
-        bossArray.splice(key, 1)
-        bossDead = true
-    }
+        if (obj2.bosscheck === false) {
+          mobArray.splice(key, 1)
+          mobsOnScreen--
+          levelOneMobCount--
+        } else if (obj2.bosscheck) {
+          bossArray.splice(key, 1)
+          bossDead = true
+        }
       }
-      if (mobsOnScreen === 0 && levelOneMobCount === 0) {
+      if (mobsOnScreen === 0 && levelOneMobCount === 0) { //if mobs are dead, spawn boss
         bossLevel = true
       }
-      if(bossLevel=== true && bossDead=== true){
+      if (bossLevel === true && bossDead === true) {   //if boss is dead, end game
         gameEnd = true
       }
       return true
     } else { return false }
   }
 
-  function playerMobCollisionCheck (obj1, obj2) {
+  function playerMobCollisionCheck (obj1, obj2) {     //player - mob collision check
     var red = obj1.position()
     var redX = red.left
     var blue = obj2.jTarget.position()
@@ -325,7 +320,7 @@ $(function () {
       console.log('PLAYER HEALTH REDUCED')
       playerHealth -= obj2.damage
       var currentHpWidth = $('.hpBar').width()
-      $('.hpBar').css('width', `${currentHpWidth - obj2.damage/5}px`)
+      $('.hpBar').css('width', `${currentHpWidth - obj2.damage / 5}px`)
       $hpBar.text(`${playerHealth}/1000`)
       if (playerHealth === 0) {
         gameEnd = true
@@ -335,7 +330,7 @@ $(function () {
     }
   }
 
-  function fireBullet () {
+  function fireBullet () {      //bullet control functiom
     $playPos = $player.position()
     $mobPos = $mob.position()
     $bullet.show()
@@ -349,32 +344,32 @@ $(function () {
     } else if (fireDirection === 'left') {
       $bullet.css('left', `${$bulletExact - (1600 - $bulletExact)}px`)
     }
-    if ($bulletExact <= 50 || $bulletExact >= 1400) {
+    if ($bulletExact <= 50 || $bulletExact >= 1400) {  //reset when it moves out of screen
       $bullet.css('left', $playPos.left)
       $bullet.hide()
       console.log('bullet', $bulletExact)
     }
   }
-  function skillOne () {
+  function skillOne () {    // small damage skill
     if (mana >= 10) {
       mana -= 10
       $mpBar.text(`${mana}/200`)
       $mpBar.css('width', `${$mpBar.width() - 10}px`)
-      $firstSkillDiv.css('visibility', 'visible')
+      $firstSkillDiv.css('visibility', 'visible') // show skill animation and hide after 800 ms
       setTimeout(function () {
         $firstSkillDiv.css('visibility', 'hidden')
       }, 800)
 
-      for (key in mobArray) {
+      for (key in mobArray) {     // test for mobs
         mobArray[key].hitPoints -= 10
         if (mobArray[key].hitPoints < 0) {
           mobArray[key].jTarget.remove()
           mobArray.splice(key, 1)
-          mobsOnScreen--
+          mobsOnScreen--        //deduct 1 from total mobs on screen
           levelOneMobCount--
         }
       }
-      for (key in bossArray) {
+      for (key in bossArray) {      // test for boss
         bossArray[key].hitPoints -= 10
         if (bossArray[key].hitPoints < 0) {
           bossArray[key].jTarget.remove()
@@ -385,19 +380,19 @@ $(function () {
       }
 
       if (mobsOnScreen === 0 && levelOneMobCount === 0) { bossLevel = true }
-      if(bossLevel=== true && bossDead=== true){gameEnd = true}
+      if (bossLevel === true && bossDead === true) { gameEnd = true }
     } else {
       return false
     }
   }
-  function skillTwo () {
+  function skillTwo () { //medium damage skill
     if (mana >= 20) {
       mana -= 20
       $mpBar.text(`${mana}/200`)
       $mpBar.css('width', `${$mpBar.width() - 20}px`)
-      $(".skillTwo").css('visibility', 'visible')
+      $('.skillTwo').css('visibility', 'visible')
       setTimeout(function () {
-        $(".skillTwo").css('visibility', 'hidden')
+        $('.skillTwo').css('visibility', 'hidden')
       }, 800)
       for (key in mobArray) {
         mobArray[key].hitPoints -= 20
@@ -419,7 +414,7 @@ $(function () {
       }
 
       if (mobsOnScreen === 0 && levelOneMobCount === 0) { bossLevel = true }
-      if(bossLevel=== true && bossDead=== true){gameEnd = true}
+      if (bossLevel === true && bossDead === true) { gameEnd = true }
     } else {
       return false
     }
@@ -435,7 +430,7 @@ $(function () {
       }, 800)
 
       for (key in mobArray) {
-        mobArray[key].hitPoints -= 50
+        mobArray[key].hitPoints -= 100
         if (mobArray[key].hitPoints < 0) {
           mobArray[key].jTarget.remove()
           mobArray.splice(key, 1)
@@ -445,7 +440,7 @@ $(function () {
       }
 
       for (key in bossArray) {
-        bossArray[key].hitPoints -= 50
+        bossArray[key].hitPoints -= 100
         if (bossArray[key].hitPoints < 0) {
           bossArray[key].jTarget.remove()
           bossArray.splice(key, 1)
@@ -455,7 +450,7 @@ $(function () {
       }
 
       if (mobsOnScreen === 0 && levelOneMobCount === 0) { bossLevel = true }
-      if(bossLevel=== true && bossDead=== true){gameEnd = true}
+      if (bossLevel === true && bossDead === true) { gameEnd = true }
     } else {
       return false
     }
