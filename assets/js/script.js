09992759522c32b2004f74b6a6df82e10de84ac6
Persis Ngan <persisngan@Persiss-MacBook-Pro.local>
2017-10-05 14:13:54 +0800

    ../../ with sounds

--- assets/js/script.js
+++ assets/js/script.js
@@ -216,9 +216,9 @@ $(function () {
   var currentAns = allQuestions[randomQnIndex].ans
   var turnCounter = 0
   var currentExp = allQuestions[randomQnIndex].exp
-  var correctSound = new Audio("../sounds/correct.wav");
-  var incorrectSound = new Audio("../sounds/wrong.wav")
-  var winSound = new Audio("../sounds/win.wav")
+  var correctSound = new Audio("../../sounds/correct.wav");
+  var incorrectSound = new Audio("../../sounds/wrong.wav")
+  var winSound = new Audio("../../sounds/win.wav")
 
   $h1.text(`${allQuestions[randomQnIndex].qn}`)
 
