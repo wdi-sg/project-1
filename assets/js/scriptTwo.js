d324414783c3def27d13df309c220dfcb1402669
siya-ng <ngsiya90@gmail.com>
2017-10-07 23:25:18 +0800

    add animation for noMove and invalid move

--- assets/js/scriptTwo.js
+++ assets/js/scriptTwo.js
@@ -16,10 +16,6 @@ var $start = $('#start')
 var toBeDeleteFinal =[]
 var $instructionBtn = $('.instructionBtn')
 var $instruction = $('.instruction')
-var iValueOne = 0
-var jValueOne = 0
-var iValueTwo = 0
-var jValueTwo = 0
 
 $(function(){
   var gameTimer = 0
@@ -32,8 +28,8 @@ $(function(){
 
   $instructionBtn.on('click',function(){
     $instruction.slideToggle(100,function(){
-      // if (settingMode) settingMode = false
-      // else settingMode = true
+      if (settingMode) settingMode = false
+      else settingMode = true
     })
   })
 
@@ -42,8 +38,10 @@ $(function(){
       $('.overText').remove()
       $('.totalScore').remove()
       $('.characterFinal').remove()
-      $character.css('background-image','url("./assets/img/yellowteeth.png")')
+      $character.css('background-image','url("./assets/css/img/yellowteeth.png")')
       gameOver = false
+      removeClassBox()
+      addClassBox()
       setGrid()
       gameTimer = setInterval(()=>{
         timer-= 1
@@ -73,18 +71,59 @@ $(function(){
     toSwitchTwo.push(idArr)
     if (toSwitchTwo.length ===2){
       setTimeout(function(){
-        iValueOne = Number(toSwitchTwo[0][3])
-        jValueOne = Number(toSwitchTwo[0][4])
-        iValueTwo = Number(toSwitchTwo[1][3])
-        jValueTwo = Number(toSwitchTwo[1][4])
+        var iValueOne = Number(toSwitchTwo[0][3])
+        var jValueOne = Number(toSwitchTwo[0][4])
+        var iValueTwo = Number(toSwitchTwo[1][3])
+        var jValueTwo = Number(toSwitchTwo[1][4])
         $(`#box${iValueOne}${jValueOne}`).css({'border':"0"})
         $(`#box${iValueTwo}${jValueTwo}`).css({'border':"0"})
         toSwitchTwo = []
         if (iValueOne === iValueTwo && (jValueTwo === jValueOne +1 || jValueTwo === jValueOne -1)){
-          switchingTwo()
+          var tempValue = grid[iValueOne][jValueOne]
+          grid[iValueOne][jValueOne] = grid[iValueTwo][jValueTwo]
+          grid[iValueTwo][jValueTwo] = tempValue
+          $(`#box${iValueOne}${jValueOne}`).removeClass()
+          $(`#box${iValueOne}${jValueOne}`).addClass(`box c${grid[iValueOne][jValueOne]}`)
+          $(`#box${iValueTwo}${jValueTwo}`).removeClass()
+          $(`#box${iValueTwo}${jValueTwo}`).addClass(`box c${grid[iValueTwo][jValueTwo]}`)
+          if (checkingMatch()){
+            setTimeout(checkGrid(),1000)
+          }
+          else{
+            setTimeout(()=>{
+              tempValue = grid[iValueTwo][jValueTwo]
+              grid[iValueTwo][jValueTwo] = grid[iValueOne][jValueOne]
+              grid[iValueOne][jValueOne] = tempValue
+              $(`#box${iValueOne}${jValueOne}`).removeClass()
+              $(`#box${iValueOne}${jValueOne}`).addClass(`box c${grid[iValueOne][jValueOne]}`)
+              $(`#box${iValueTwo}${jValueTwo}`).removeClass()
+              $(`#box${iValueTwo}${jValueTwo}`).addClass(`box c${grid[iValueTwo][jValueTwo]}`)
+            },250)
+          }
         }
         else if (jValueOne === jValueTwo && (iValueTwo === iValueOne +1 || iValueTwo === iValueOne -1)){
-          switchingTwo()
+          var tempValue = grid[iValueOne][jValueOne]
+          grid[iValueOne][jValueOne] = grid[iValueTwo][jValueTwo]
+          grid[iValueTwo][jValueTwo] = tempValue
+          $(`#box${iValueOne}${jValueOne}`).removeClass()
+          $(`#box${iValueOne}${jValueOne}`).addClass(`box c${grid[iValueOne][jValueOne]}`)
+          $(`#box${iValueTwo}${jValueTwo}`).removeClass()
+          $(`#box${iValueTwo}${jValueTwo}`).addClass(`box c${grid[iValueTwo][jValueTwo]}`)
+          if (checkingMatch()){
+            setTimeout(checkGrid(),1000)
+          }
+          else{
+            setTimeout(()=>{
+              tempValue = grid[iValueTwo][jValueTwo]
+              grid[iValueTwo][jValueTwo] = grid[iValueOne][jValueOne]
+              grid[iValueOne][jValueOne] = tempValue
+              $(`#box${iValueOne}${jValueOne}`).removeClass()
+              $(`#box${iValueOne}${jValueOne}`).addClass(`box c${grid[iValueOne][jValueOne]}`)
+              $(`#box${iValueTwo}${jValueTwo}`).removeClass()
+              $(`#box${iValueTwo}${jValueTwo}`).addClass(`box c${grid[iValueTwo][jValueTwo]}`)
+            },250)
+          }
+
         }
         else {
            $(`#box${iValueOne}${jValueOne}`).effect("bounce", { times: 3 }, 150 )
@@ -114,7 +153,7 @@ $(function(){
         $(`#box${iValueTwo}${jValueTwo}`).removeClass()
         $(`#box${iValueTwo}${jValueTwo}`).addClass(`box c${grid[iValueTwo][jValueTwo]}`)
       },250)
-    }
+    }    
   }
   function noAvailableMove  (){
     $gridbox.css({"filter": "grayscale(1)"})
@@ -180,7 +219,7 @@ $(function(){
       for (var i = 0; i< gridNo;i++){
         for(var j = 0; j<gridNo;j++){
           if (grid[i][j] === undefined){
-            var elementValue = Math.ceil(Math.random()*5)
+            var elementValue = Math.ceil(Math.random()*8)
             var $oneBox = $(`#box${i}${j}`)
             $oneBox.addClass(`c${elementValue}`)
             grid[i][j] = elementValue
@@ -229,6 +268,7 @@ $(function(){
         // alert('No more moves. Resetting the grid')
         noAvailableMove()
       }
+      else return false
     }
   }
   function finaliseRemoveList(){
@@ -271,16 +311,33 @@ $(function(){
   }
   function pushDown(){
     for (var i = 0; i< gridNo; i++){
-      for(var j = gridNo-1; j > 0 ;j--){
-        if (grid[i][j]=== undefined){
-          $oneBox = $(`#box${i}${j}`)
-          for (var k = j-1; k >= 0 ;k--){
-            if(grid[i][k]!==undefined || grid[i][k]){
-              grid[i][j] = grid[i][k]
+      for(var k = gridNo-1; k >= 0 ;k--){
+        if (grid[i][k] === undefined){
+          $oneBox = $(`#box${i}${k}`)
+          if(k > 0){
+            if (grid[i][k-1]!== undefined||grid[i][k-1]){
+              grid[i][k] = grid[i][k-1]
               $oneBox.addClass(`c${grid[i][k]}`)
-              grid[i][k] = undefined
-              $(`#box${i}${k}`).removeClass().addClass('box')
-              break
+              grid[i][k-1] = undefined
+              $(`#box${i}${k-1}`).removeClass().addClass('box')
+            }
+            else if(grid[i][k-2]!== undefined||grid[i][k-2]){
+              grid[i][k] = grid[i][k-2]
+              $oneBox.addClass(`c${grid[i][k]}`)
+              grid[i][k-2] = undefined
+              $(`#box${i}${k-2}`).removeClass().addClass('box')
+            }
+            else if(grid[i][k-3]!== undefined || grid[i][k-3]){
+              grid[i][k] = grid[i][k-3]
+              $oneBox.addClass(`c${grid[i][k]}`)
+              grid[i][k-3] = undefined
+              $(`#box${i}${k-3}`).removeClass().addClass('box')
+            }
+            else if (grid[i][k-4]!== undefined || grid[i][k-4]){
+              grid[i][k] = grid[i][k-4]
+              $oneBox.addClass(`c${grid[i][k]}`)
+              grid[i][k-4] = undefined
+              $(`#box${i}${k-4}`).removeClass().addClass('box')
             }
           }
         }
