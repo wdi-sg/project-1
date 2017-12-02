d3ad462c034b3c1b1941215167253e89a4debe4b
Edmund Teo <Edmund@edmunds-mac.mbox.com>
2017-12-02 20:58:10 +0800

    level 25 feature with reset

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@
 // if false popup exploded, button to Retry
 // button to level 2 the loop
 var minutes, seconds, sequenceInPlay, playerSequence, interval;
-var timer = 30;
+var timer;
 var level = 1;
 var gameSource = {
   images: {
@@ -27,12 +27,12 @@ var gameSource = {
     blue: 'images/project1-triangle-blue.png'
   },
   seqCombination: [
-  ['red', 'green', 'blue'],
-  ['red', 'blue', 'green'],
-  ['green', 'red', 'blue'],
-  ['green', 'blue', 'red'],
-  ['blue', 'red', 'green'],
-  ['blue', 'green', 'red'],
+    ['red', 'green', 'blue'],
+    ['red', 'blue', 'green'],
+    ['green', 'red', 'blue'],
+    ['green', 'blue', 'red'],
+    ['blue', 'red', 'green'],
+    ['blue', 'green', 'red'],
   ],
   level1: {
     red: 1,
@@ -51,15 +51,159 @@ var gameSource = {
     green: 1,
     blue: 3,
     timing: 5
-  }
+  },
+  level4: {
+    red: 3,
+    green: 1,
+    blue: 2,
+    timing: 10
+  },
+  level5: {
+    red: 3,
+    green: 1,
+    blue: 2,
+    timing: 10
+  },
+  level6: {
+    red: 2,
+    green: 1,
+    blue: 3,
+    timing: 10
+  },
+  level7: {
+    red: 3,
+    green: 1,
+    blue: 2,
+    timing: 15
+  },
+  level8: {
+    red: 3,
+    green: 1,
+    blue: 2,
+    timing: 15
+  },
+  level9: {
+    red: 2,
+    green: 3,
+    blue: 1,
+    timing: 15
+  },
+  level10: {
+    red: 2,
+    green: 1,
+    blue: 3,
+    timing: 20
+  },
+  level11: {
+    red: 3,
+    green: 2,
+    blue: 1,
+    timing: 20
+  },
+  level12: {
+    red: 1,
+    green: 2,
+    blue: 3,
+    timing: 20
+  },
+  level13: {
+    red: 2,
+    green: 1,
+    blue: 3,
+    timing: 30
+  },
+  level14: {
+    red: 1,
+    green: 3,
+    blue: 2,
+    timing: 30
+  },
+  level15: {
+    red: 2,
+    green: 1,
+    blue: 3,
+    timing: 30
+  },
+  level16: {
+    red: 3,
+    green: 2,
+    blue: 1,
+    timing: 40
+  },
+  level17: {
+    red: 2,
+    green: 3,
+    blue: 1,
+    timing: 40
+  },
+  level18: {
+    red: 1,
+    green: 3,
+    blue: 2,
+    timing: 40
+  },
+  level19: {
+    red: 3,
+    green: 2,
+    blue: 1,
+    timing: 50
+  },
+  level20: {
+    red: 3,
+    green: 1,
+    blue: 2,
+    timing: 50
+  },
+  level21: {
+    red: 2,
+    green: 3,
+    blue: 1,
+    timing: 50
+  },
+  level22: {
+    red: 3,
+    green: 2,
+    blue: 1,
+    timing: 60
+  },
+  level23: {
+    red: 2,
+    green: 1,
+    blue: 3,
+    timing: 60
+  },
+  level24: {
+    red: 3,
+    green: 1,
+    blue: 2,
+    timing: 60
+  },
+  level25: {
+    red: 1,
+    green: 2,
+    blue: 3,
+    timing: 60
+  },
 };
 
 
 function switchPlaySequence() {
-var random = Math.floor(Math.random * 6);
+  var random = Math.floor(Math.random * 6);
 
 
-  }
+}
+
+
+function restart() {
+  sequenceInPlay = [];
+  playerSequence = [];
+  level = 1;
+  $('.timer-countdown').text('Focus your way through the lines and press the button according to the sequence');
+  $('.timer-countdown').css('font-size', '1rem');
+  $('.buttons').empty();
+  $('.buttons').append('<button class="btn-new-game">New Game</button>');
+  $('img').attr('src', 'images/project1-26.jpg');
+}
 
 
 
@@ -67,31 +211,32 @@ var random = Math.floor(Math.random * 6);
 function randomInPlay() {
   sequenceInPlay = [];
   playerSequence = [];
+  timer = gameSource['level' + level].timing;
+  var random = Math.floor(Math.random() * 6);
+  console.log(random);
+  var combination = gameSource.seqCombination[random];
+  console.log(combination);
+  var sequence = gameSource['level' + level];
+  var src;
+
   $('.btn').text('Open');
   $('.triangle-seq').empty();
   $('.btn').css('background-color', 'grey');
-var random = Math.floor(Math.random() * 6);
-console.log(random);
-var combination = gameSource.seqCombination[random];
-console.log(combination);
-  var sequence = gameSource['level' + level];
-for (var i of combination) {
-  sequenceInPlay.push(sequence[i]);
-}
-
-
-  console.log('In play ' + sequenceInPlay);
   $('.timer-countdown').text('00:00');
   $('img').attr('src', 'images/project1-' + level + '.jpg');
   $('.modal-content h1').text('Level ' + level);
 
   for (var i of combination) {
-    var src = gameSource.images[i];
-    console.log(src);
-  $('.triangle-seq').append(`<img class="triangle" src="${src}">`);
-}
+    sequenceInPlay.push(sequence[i]);
+  }
+  console.log('In play ' + sequenceInPlay);
 
-  timer = gameSource['level' + level].timing;
+  for (var i of combination) {
+    src = gameSource.images[i];
+    console.log(src);
+    $('.triangle-seq').append(`<img class="triangle" src="${src}">`);
+  }
+
   $('.modal').show();
 }
 
@@ -106,13 +251,17 @@ $('.nextOrRetry').click(function() {
     level++;
     $('.modal2').hide();
     randomInPlay();
+  } else if (checkStatus == 2) {
+    console.log('End Game');
+    $('.modal2').hide();
+    restart();
   } else {
     // retry
     console.log('Retry');
     $('.modal2').hide();
     randomInPlay();
   }
-  console.log('data 0??');
+  console.log('reset data-id');
   $('.nextOrRetry').attr('data-id', 0);
 });
 
@@ -121,7 +270,8 @@ $('.nextOrRetry').click(function() {
 
 
 // click new game - img change, random sequence, show modal, remove new game button, show timer, show level and sequence to play
-$('.btn-new-game').click(function() {
+
+$('.buttons').on('click', '.btn-new-game', function() {
   $('.timer-countdown').text('00:00');
   $('.timer-countdown').css('font-size', '2rem');
   $('.buttons').empty();
@@ -135,9 +285,9 @@ $('.btn-new-game').click(function() {
 
 
 // hide for lets go
-$('.close').click(function() {
-  $('.modal').hide();
-});
+// $('.close').click(function() {
+//   $('.modal').hide();
+// });
 
 
 
@@ -147,7 +297,6 @@ function checkForWin(inPlay, player) {
   for (var i = 0; i < 3; i++) {
     if (inPlay[i] != player[i]) {
       clearInterval(interval);
-
       console.log('Exploded');
       return false;
     }
@@ -162,7 +311,7 @@ function checkForWin(inPlay, player) {
 // adding on click event to the newly created 3 button
 // check when all three button are press in sequence
 $('.buttons').on('click', '.btn', function() {
-$(this).css('background-color', '#5D6D7E');
+  $(this).css('background-color', '#5D6D7E');
   var dataId = $(this).attr('data-id');
   $(this).text('Lock');
   console.log('dataId is ' + dataId);
@@ -172,9 +321,15 @@ $(this).css('background-color', '#5D6D7E');
     var winOrLose = checkForWin(sequenceInPlay, playerSequence);
 
     if (winOrLose) {
-      $('.modal-content2 h1').text('Hooray!! Well Done!!');
+
+        $('.modal-content2 h1').html('Hooray!!<br>Well Done!!');
       $('.nextOrRetry').text('Next Level');
       $('.nextOrRetry').attr('data-id', 1);
+      if (level == 25){
+        $('.modal-content2 h1').html('Congratulations!!<br>Thank you for playing!!');
+      $('.nextOrRetry').text('End Game');
+      $('.nextOrRetry').attr('data-id', 2);
+      }
       $('.modal2').show();
     } else {
       $('.modal-content2 h1').text('Oh No!!');
@@ -187,7 +342,7 @@ $(this).css('background-color', '#5D6D7E');
 
 
 $('.timer').click(function() {
-
+$('.modal').hide();
   interval = setInterval(function() {
     if (timer >= 60) {
       minutes = Math.floor(timer / 60);
