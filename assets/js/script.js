89c6e3af768518bf992aa0ab4d7e64472d810827
Lim Jiechao <jiechao.lim@gmail.com>
2017-12-05 21:33:32 +0800

    improved comments in script.js and tidied up stylesheet.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,8 @@
 /*
 Terms used:
-- Canvas, Colomn, Row
-- Flood
-- Fill color, old fill color, new fill color
-- Color Scheme
-- Palette
+- Canvas, canvas length, colomn, row, 
+- Flood, fill color, old fill color, new fill color
+- Color Scheme, palette
 */
 $(document).ready(function() {
 	var colorSchemes = [
@@ -20,7 +18,7 @@ $(document).ready(function() {
 	var oldFillColor, newFillColor;
 	var movesLeft;
 
-	// Initialize or reinitialize the game
+	// Initializes or reinitializes the game
 	function restartGame() {
 		colorScheme = colorSchemes[0];
 		canvasLength = 15;
@@ -31,13 +29,14 @@ $(document).ready(function() {
 	}
 
 
+	// Creates the entire canvas according to length of canvas specified with each box given a randomly selected color from the chosen color scheme and stores it in a two-dimensional array
 	function createCanvas(length) {
 		// Creates an array to store arrays of columns in the canvas
 		var canvasArray = [];
 		for (var column = 0; column < length; column++) {
-			// Create an array to store the boxes in each column
+			// Creates an array to store the boxes in each column
 			var columnArray = [];
-			// Choose a random color for each box and append to the column array
+			// Chooses a random color for each box and append to the column array
 			for (var box = 0; box < length; box++) {
 				columnArray.push(colorScheme[Math.floor(Math.random() * 6)]);
 			}
@@ -48,10 +47,10 @@ $(document).ready(function() {
 		return canvasArray;
 	}
 
-		
-	// Create the entire game canvas according to length of canvas specified
+	
+	// Draws the canvas from the two-dimensional array and outputs to HTML for display
 	function drawCanvas(canvas, length) {
-		// Remove old canvas before drawing new canvas
+		// Removes old canvas before drawing new canvas
 		$("#canvas").remove();
 		// Creates canvas and appends in container
 		$("#canvasContainer").append("<div id=\"canvas\"></div>");
@@ -67,9 +66,9 @@ $(document).ready(function() {
 	}
 
 
-	// Append the colors in the selected color scheme below the canvas
+	// Appends the colors in the selected color scheme below the canvas
 	function drawPalette(colorScheme) {
-		// Run a loop to append the colors into CSS circles
+		// Runs a loop to append the colors into CSS circles
 		for (color = 0; color < colorScheme.length; color++) {
 			// Creates the circle and tags it with `id` and `color` attributes
 			$("#palette").append("<div class=\"circle\" id=\"color-" + color + "\" color=\"" + colorScheme[color] + "\"></div>")
@@ -79,18 +78,18 @@ $(document).ready(function() {
 	}
 
 
-	// Existing flood will merge in with boxes in the newly choosen fill color
+	// Existing flood merges with boxes already in newly choosen fill color by changing to it, thus expanding the flood
 	function expandFlood(canvas, oldFillColor, newFillColor, xCoordinate, yCoordinate) {
 		/*
 		Before changing those already in the flood to the new fill color, stop and exit this function if
-			1. old fill color is the same as the new fill color, i.e. player clicked on old fill color, or
-			2. current box is not already in the old fill color, then it is not part of the flood. This means the check has reached the boundary of the flood
+			1. old fill color is the same as the new fill color, i.e. player clicked and selected the old fill color, or
+			2. current box is not already in the old fill color, which means it is not part of the flood. This means the check has reached the boundaries of the flood
 		*/
 		if (oldFillColor == newFillColor || canvas[xCoordinate][yCoordinate] != oldFillColor) {
 			console.log("Box at [x: " + xCoordinate + "][y: " + yCoordinate + "] is either already in new fill color or just beyond the flooded area")
 			return;
 		} else {
-			// The current box has been verified to be part of the flood and not already in the new flood color. Hence, it is now changed to the new fill color.
+			// The current box has been verified to be part of the flood and not already in the new flood color. Here, it is assigned to the new fill color.
 			canvas[xCoordinate][yCoordinate] = newFillColor;
 			console.log("Changing box at [x: " + xCoordinate + "][y: " + yCoordinate + "] to new fill color " + canvas[xCoordinate][yCoordinate]);
 			// If current box is not at the left edge, then pass the box on its left as an argument into `expandFlood(…)` to locate boxes in the flood until all have been changed to new fill color
@@ -116,15 +115,14 @@ $(document).ready(function() {
 		}
 	}
 
-
+	// Reduces the move by one after every valid color chosen
 	function updateMoves() {
 		// If player accidentally clicks on the old fill color, no moves will be deducted. If a valid move (i.e., choosing a different fill color) is made, moves will reduce by 1
 		newFillColor != oldFillColor ? movesLeft-- : movesLeft = movesLeft;
 	}
 
-
+	// Checks if there are moves left and returns true or false
 	function anyMovesLeft() {
-		// If there are no more moves left, game will be terminated
 		if (movesLeft != 0) {
 			return true;
 		} else {
@@ -134,24 +132,24 @@ $(document).ready(function() {
 
 	// Checks if whole canvas has same color, otherwise game continues.
 	function checkIfWon(canvas) {
-		// Iterate through every column in the canvas array
+		// Iterates through every column in the canvas array
 		for (var column = 0; column < canvas.length; column++) {
-			// Interate through every box in each column
+			// Interates through every box in each column
 			for (var row = 0; row < canvas.length; row++) {
-				// If color of current box is not the same to the current fill color, canvas still has more than one color, therefore terminate the loop and return false.
+				// If color of current box is not the same to the current fill color, then the canvas still has more than one color, therefore terminate the loop and return `false`.
 				if (canvas[column][row] != canvas[0][0]) {
 					console.log("Canvas has more than one color, game continues…");
 					return false;
 				}
 			}
 		}
-		// After fully iterating through every column and every box without finding any box different from the current fill color, canvas is verfied to be filled with the same color. Therefore, return true.
+		// After fully iterating through every column and every box without finding any box different from the current fill color, canvas is verfied to be filled with the same color. Therefore, return `true`.
 		console.log("Canvas is filled with the same color, player has won!");
 		return true;
 	}
 
 
-	// Loads victory sequence if player has won and defeat sequence if player has lost
+	// Loads victory sequence if player has won or the defeat sequence if player has lost
 	function endGameSequence(hasPlayerWon) {
 		if (hasPlayerWon == true) {
 			setTimeout(function(){ $("#title").text("You did it!!!"); }, 500);
@@ -164,11 +162,11 @@ $(document).ready(function() {
 		setTimeout(function(){ $("#title").addClass("flicker"); }, 500);
 	}
 
-	// Determines whether to stop or continue game according to whether there are moves left and whether player has won the game
+	// Determines whether to stop or continue the game according to whether there are moves left and whether player has won the game
 	function continueOrStopGame() {
 		// If there are moves left and player has not won the game
 		if (anyMovesLeft() == true && checkIfWon(canvas) == false) {
-			// Do nothing and exit this function to continue with the game
+			// Does nothing and exits this function to continue with the game
 			return;
 		} else {
 			/*
@@ -182,23 +180,23 @@ $(document).ready(function() {
 	}
 
 
-	
+	// Event listener for color that the player clicks
 	$("#colorPickerZone").on("click", function(event) {
-		// Stop the flicker animation on game title once game starts
+		// Stops the flicker animation on game title once the game starts
 		$("#title").removeClass("flicker");
-		// Fade out and hide the game instruction onde game starts
+		// Fades out and hides the game instruction once the game starts
 		$("#instruction").fadeOut(200);
 
-		// Grab the `color` attribute whether player clicks on the palette or a box within the canvas
+		// Grabs the `color` attribute whether the player clicks on the palette or a box within the canvas
 		colorClicked = $(event.target).attr("color");
 
-		// If player did not click on a color in the palette or a box in the canvas, do nothing.
+		// If player does not click on a color in the palette or a box in the canvas, then do nothing.
 		if (colorClicked == undefined) {
 			console.log("No color clicked, execution stops here")
 			return;
 		}
 
-		// Previous fill color can always be found in the top left box
+		// Previous fill color can always be found in the top left box and thus assigned to old fill color
 		oldFillColor = canvas[0][0];
 		console.log("Old fill color is " + canvas[0][0]);
 
@@ -207,20 +205,21 @@ $(document).ready(function() {
 		console.log("New fill color is " + newFillColor);
 
 		console.log("Calling expandFlood(…)");
-		// Change the flood to new fill color and incorporate those already in new fill color
+		// Changes the flood to new fill color, thereby incorporating those already in new fill color
 		expandFlood(canvas, oldFillColor, newFillColor, 0, 0);
 		console.log("Returning from expandFlood(…)");
 
-		// Draw canvas with new fill color and expanded flood
+		// Draws canvas with new fill color and expanded flood to HTML for display
 		drawCanvas(canvas, canvasLength);
 
-		// Update moves left after a color has been clicked and check if moves are all used up
+		// Updates moves left after a color has been clicked
 		updateMoves();
 		console.log("Moves left: " + movesLeft);
 
-		// Continue or stop the game according to moves left and whether player has completed the game
+		// Continues or stops the game according to the moves left and whether player has completed the game
 		continueOrStopGame();
 	});
 
+	// This initializes the game for the first time upon page load
 	restartGame();
 });
