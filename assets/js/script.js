b4d5cf88204ad8a2535dd720e4ad395fe7e53090
chongct <chongchingtong@gmail.com>
2017-12-07 11:08:54 +0800

    gh pages test2

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,6 @@ Pseudo code:
 6. Check for win.
 7. Next stage feature.
 8. Undo move feature.
-9. test.
 */
 var playerGrid = document.querySelector(".container-player");
 var comGrid = document.querySelector(".container-com");
